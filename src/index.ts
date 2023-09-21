bd3ae50184e8ebeeabba16b4da645dc8b4ffe91b
8845musign <hiroki.yokouchi@dr-ubie.com>
2023-09-21 09:45:31 +0900

    Delete components of specific products
    Signed-off-by: 8845musign <hiroki.yokouchi@dr-ubie.com>

--- src/index.ts
+++ src/index.ts
@@ -6,6 +6,3 @@ export { LinkCard } from './components/LinkCard/LinkCard';
 export { RadioCard } from './components/RadioCard/RadioCard';
 export { Text } from './components/Text/Text';
 export { Stack } from './components/Stack/Stack';
-export { DiseaseTextAdRichButton } from './components/DiseaseTextAd/DiseaseTextAdRichButton';
-export { DiseaseTextAdSummaryButton } from './components/DiseaseTextAd/DiseaseTextAdSummaryButton';
-export { DiseaseAdText } from './components/DiseaseTextAd/DiseaseAdText';
