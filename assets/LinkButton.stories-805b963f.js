05e5f15e832d986ac317978e03a4ef5809ed5b89
takanorip <takanorip@users.noreply.github.com>
2023-09-26 14:05:53 +0000

    deploy: 937d9087e1122efe52dab67322aac019036a31ac

---
+++ assets/LinkButton.stories-805b963f.js
@@ -0,0 +1,183 @@
+import{j as l,F as E,a as i}from"./jsx-runtime-03b4ddbf.js";import{S as a,a as e}from"./UbieIcon-89f929be.js";import{c as S}from"./clsx-1229b3e0.js";import{r as L}from"./index-76fb7be0.js";import{s,V as c}from"./VariantIcon-22342d2d.js";import"./_commonjsHelpers-de833af9.js";const n=L.forwardRef(({render:t,children:g,variant:r="primary",size:b="large",className:h,block:D=!1,icon:d,fixedIcon:f,suffixIcon:x,href:w,disabled:m=!1,...U},A)=>{const u=d==="default"?i(c,{variant:r}):d,v=f==="default"?i(c,{variant:r}):f,k=x==="default"?i(c,{variant:r}):x,N=S({[s.button]:!0,[s[r]]:!0,[s[b]]:!0,[s.block]:D,[s.disabled]:m},h);return((I,p)=>t?L.cloneElement(t,I,p):i("a",{...I,children:p}))({className:N,href:m?void 0:w,...U,ref:A},l(E,{children:[v&&i("span",{className:s.fixedIcon,children:v}),l("span",{className:s.label,children:[u&&i("span",{className:s.icon,children:u}),g,k&&i("span",{className:s.suffixIcon,children:k})]})]}))});n.displayName="Link";const J={component:n},j={children:"ボタン",href:"https://example.com"},o={render:t=>l("div",{style:{display:"flex",flexDirection:"column",gap:"48px"},children:[l("div",{style:{display:"flex",flexDirection:"row",alignItems:"flex-start",gap:"32px"},children:[i(n,{icon:i(a,{}),...t}),i(n,{icon:i(a,{}),...t,variant:"secondary"}),i(n,{icon:i(a,{}),...t,variant:"accent"}),i(n,{icon:i(a,{}),...t,variant:"alert"}),i(n,{icon:i(a,{}),...t,variant:"text"}),i(n,{icon:i(a,{}),...t,variant:"textAlert"})]}),l("div",{style:{display:"flex",flexDirection:"row",alignItems:"flex-start",gap:"32px"},children:[i(n,{icon:i(a,{}),...t,size:"medium"}),i(n,{icon:i(a,{}),...t,size:"medium",variant:"secondary"}),i(n,{icon:i(a,{}),...t,size:"medium",variant:"accent"}),i(n,{icon:i(a,{}),...t,size:"medium",variant:"alert"}),i(n,{icon:i(a,{}),...t,size:"medium",variant:"text"}),i(n,{icon:i(a,{}),...t,size:"medium",variant:"textAlert"})]}),l("div",{style:{display:"flex",flexDirection:"row",alignItems:"flex-start",gap:"32px"},children:[i(n,{icon:i(a,{}),...t,size:"small"}),i(n,{icon:i(a,{}),...t,size:"small",variant:"secondary"}),i(n,{icon:i(a,{}),...t,size:"small",variant:"accent"}),i(n,{icon:i(a,{}),...t,size:"small",variant:"alert"}),i(n,{icon:i(a,{}),...t,size:"small",variant:"text"}),i(n,{icon:i(a,{}),...t,size:"small",variant:"textAlert"})]}),l("div",{style:{display:"flex",flexDirection:"row",alignItems:"flex-start",gap:"32px"},children:[i(n,{suffixIcon:i(e,{}),...t}),i(n,{suffixIcon:i(e,{}),...t,variant:"secondary"}),i(n,{suffixIcon:i(e,{}),...t,variant:"accent"}),i(n,{suffixIcon:i(e,{}),...t,variant:"alert"}),i(n,{suffixIcon:i(e,{}),...t,variant:"text"})]}),l("div",{style:{display:"flex",flexDirection:"row",alignItems:"flex-start",gap:"32px"},children:[i(n,{suffixIcon:i(e,{}),size:"medium",...t}),i(n,{suffixIcon:i(e,{}),size:"medium",...t,variant:"secondary"}),i(n,{suffixIcon:i(e,{}),size:"medium",...t,variant:"accent"}),i(n,{suffixIcon:i(e,{}),size:"medium",...t,variant:"alert"}),i(n,{suffixIcon:i(e,{}),size:"medium",...t,variant:"text"})]}),l("div",{style:{display:"flex",flexDirection:"row",alignItems:"flex-start",gap:"32px"},children:[i(n,{suffixIcon:i(e,{}),size:"small",...t}),i(n,{suffixIcon:i(e,{}),size:"small",...t,variant:"secondary"}),i(n,{suffixIcon:i(e,{}),size:"small",...t,variant:"accent"}),i(n,{suffixIcon:i(e,{}),size:"small",...t,variant:"alert"}),i(n,{suffixIcon:i(e,{}),size:"small",...t,variant:"text"})]}),l("div",{style:{display:"flex",flexDirection:"row",alignItems:"flex-start",gap:"32px"},children:[i(n,{fixedIcon:i(e,{}),...t}),i(n,{fixedIcon:i(e,{}),...t,variant:"secondary"}),i(n,{fixedIcon:i(e,{}),...t,variant:"accent"}),i(n,{fixedIcon:i(e,{}),...t,variant:"alert"}),i(n,{fixedIcon:i(e,{}),...t,variant:"text"})]}),l("div",{style:{display:"flex",flexDirection:"row",alignItems:"flex-start",gap:"32px"},children:[i(n,{fixedIcon:i(e,{}),size:"medium",...t}),i(n,{fixedIcon:i(e,{}),size:"medium",...t,variant:"secondary"}),i(n,{fixedIcon:i(e,{}),size:"medium",...t,variant:"accent"}),i(n,{fixedIcon:i(e,{}),size:"medium",...t,variant:"alert"}),i(n,{fixedIcon:i(e,{}),size:"medium",...t,variant:"text"})]}),l("div",{style:{display:"flex",flexDirection:"row",alignItems:"flex-start",gap:"32px"},children:[i(n,{fixedIcon:i(e,{}),size:"small",...t}),i(n,{fixedIcon:i(e,{}),size:"small",...t,variant:"secondary"}),i(n,{fixedIcon:i(e,{}),size:"small",...t,variant:"accent"}),i(n,{fixedIcon:i(e,{}),size:"small",...t,variant:"alert"}),i(n,{fixedIcon:i(e,{}),size:"small",...t,variant:"text"})]}),l("div",{style:{display:"flex",flexDirection:"row",alignItems:"flex-start",gap:"32px"},children:[i(n,{...t}),i(n,{...t,variant:"secondary"}),i(n,{...t,variant:"accent"}),i(n,{...t,variant:"alert"}),i(n,{...t,variant:"text"}),i(n,{...t,variant:"textAlert"})]}),l("div",{style:{display:"flex",flexDirection:"row",alignItems:"flex-start",gap:"32px"},children:[i(n,{...t,size:"medium"}),i(n,{...t,size:"medium",variant:"secondary"}),i(n,{...t,size:"medium",variant:"accent"}),i(n,{...t,size:"medium",variant:"alert"}),i(n,{...t,size:"medium",variant:"text"}),i(n,{...t,size:"medium",variant:"textAlert"})]}),l("div",{style:{display:"flex",flexDirection:"row",alignItems:"flex-start",gap:"32px"},children:[i(n,{...t,size:"small"}),i(n,{...t,size:"small",variant:"secondary"}),i(n,{...t,size:"small",variant:"accent"}),i(n,{...t,size:"small",variant:"alert"}),i(n,{...t,size:"small",variant:"text"}),i(n,{...t,size:"small",variant:"textAlert"})]}),l("div",{style:{display:"flex",flexDirection:"column",alignItems:"flex-start",gap:"32px"},children:[i(n,{...t,size:"small",block:!0,variant:"secondary"}),i(n,{...t,size:"medium",block:!0,variant:"secondary"}),i(n,{...t,size:"large",block:!0,variant:"secondary"})]}),l("div",{style:{display:"flex",flexDirection:"row",alignItems:"flex-start",gap:"32px"},children:[i(n,{href:"https://vitals.ubie.life/",...t,disabled:!0}),i(n,{href:"https://vitals.ubie.life/",...t,variant:"secondary",disabled:!0}),i(n,{href:"https://vitals.ubie.life/",...t,variant:"accent",disabled:!0}),i(n,{href:"https://vitals.ubie.life/",...t,variant:"alert",disabled:!0}),i(n,{href:"https://vitals.ubie.life/",...t,variant:"text",disabled:!0}),i(n,{href:"https://vitals.ubie.life/",...t,variant:"textAlert",disabled:!0})]})]}),args:j};var B,y,z;o.parameters={...o.parameters,docs:{...(B=o.parameters)==null?void 0:B.docs,source:{originalSource:`{
+  render: args => <div style={{
+    display: 'flex',
+    flexDirection: 'column',
+    gap: '48px'
+  }}>
+      <div style={{
+      display: 'flex',
+      flexDirection: 'row',
+      alignItems: 'flex-start',
+      gap: '32px'
+    }}>
+        <LinkButton icon={<UbieIcon />} {...args} />
+        <LinkButton icon={<UbieIcon />} {...args} variant="secondary" />
+        <LinkButton icon={<UbieIcon />} {...args} variant="accent" />
+        <LinkButton icon={<UbieIcon />} {...args} variant="alert" />
+        <LinkButton icon={<UbieIcon />} {...args} variant="text" />
+        <LinkButton icon={<UbieIcon />} {...args} variant="textAlert" />
+      </div>
+      <div style={{
+      display: 'flex',
+      flexDirection: 'row',
+      alignItems: 'flex-start',
+      gap: '32px'
+    }}>
+        <LinkButton icon={<UbieIcon />} {...args} size="medium" />
+        <LinkButton icon={<UbieIcon />} {...args} size="medium" variant="secondary" />
+        <LinkButton icon={<UbieIcon />} {...args} size="medium" variant="accent" />
+        <LinkButton icon={<UbieIcon />} {...args} size="medium" variant="alert" />
+        <LinkButton icon={<UbieIcon />} {...args} size="medium" variant="text" />
+        <LinkButton icon={<UbieIcon />} {...args} size="medium" variant="textAlert" />
+      </div>
+      <div style={{
+      display: 'flex',
+      flexDirection: 'row',
+      alignItems: 'flex-start',
+      gap: '32px'
+    }}>
+        <LinkButton icon={<UbieIcon />} {...args} size="small" />
+        <LinkButton icon={<UbieIcon />} {...args} size="small" variant="secondary" />
+        <LinkButton icon={<UbieIcon />} {...args} size="small" variant="accent" />
+        <LinkButton icon={<UbieIcon />} {...args} size="small" variant="alert" />
+        <LinkButton icon={<UbieIcon />} {...args} size="small" variant="text" />
+        <LinkButton icon={<UbieIcon />} {...args} size="small" variant="textAlert" />
+      </div>
+      <div style={{
+      display: 'flex',
+      flexDirection: 'row',
+      alignItems: 'flex-start',
+      gap: '32px'
+    }}>
+        <LinkButton suffixIcon={<BlankLinkIcon />} {...args} />
+        <LinkButton suffixIcon={<BlankLinkIcon />} {...args} variant="secondary" />
+        <LinkButton suffixIcon={<BlankLinkIcon />} {...args} variant="accent" />
+        <LinkButton suffixIcon={<BlankLinkIcon />} {...args} variant="alert" />
+        <LinkButton suffixIcon={<BlankLinkIcon />} {...args} variant="text" />
+      </div>
+      <div style={{
+      display: 'flex',
+      flexDirection: 'row',
+      alignItems: 'flex-start',
+      gap: '32px'
+    }}>
+        <LinkButton suffixIcon={<BlankLinkIcon />} size="medium" {...args} />
+        <LinkButton suffixIcon={<BlankLinkIcon />} size="medium" {...args} variant="secondary" />
+        <LinkButton suffixIcon={<BlankLinkIcon />} size="medium" {...args} variant="accent" />
+        <LinkButton suffixIcon={<BlankLinkIcon />} size="medium" {...args} variant="alert" />
+        <LinkButton suffixIcon={<BlankLinkIcon />} size="medium" {...args} variant="text" />
+      </div>
+      <div style={{
+      display: 'flex',
+      flexDirection: 'row',
+      alignItems: 'flex-start',
+      gap: '32px'
+    }}>
+        <LinkButton suffixIcon={<BlankLinkIcon />} size="small" {...args} />
+        <LinkButton suffixIcon={<BlankLinkIcon />} size="small" {...args} variant="secondary" />
+        <LinkButton suffixIcon={<BlankLinkIcon />} size="small" {...args} variant="accent" />
+        <LinkButton suffixIcon={<BlankLinkIcon />} size="small" {...args} variant="alert" />
+        <LinkButton suffixIcon={<BlankLinkIcon />} size="small" {...args} variant="text" />
+      </div>
+      <div style={{
+      display: 'flex',
+      flexDirection: 'row',
+      alignItems: 'flex-start',
+      gap: '32px'
+    }}>
+        <LinkButton fixedIcon={<BlankLinkIcon />} {...args} />
+        <LinkButton fixedIcon={<BlankLinkIcon />} {...args} variant="secondary" />
+        <LinkButton fixedIcon={<BlankLinkIcon />} {...args} variant="accent" />
+        <LinkButton fixedIcon={<BlankLinkIcon />} {...args} variant="alert" />
+        <LinkButton fixedIcon={<BlankLinkIcon />} {...args} variant="text" />
+      </div>
+      <div style={{
+      display: 'flex',
+      flexDirection: 'row',
+      alignItems: 'flex-start',
+      gap: '32px'
+    }}>
+        <LinkButton fixedIcon={<BlankLinkIcon />} size="medium" {...args} />
+        <LinkButton fixedIcon={<BlankLinkIcon />} size="medium" {...args} variant="secondary" />
+        <LinkButton fixedIcon={<BlankLinkIcon />} size="medium" {...args} variant="accent" />
+        <LinkButton fixedIcon={<BlankLinkIcon />} size="medium" {...args} variant="alert" />
+        <LinkButton fixedIcon={<BlankLinkIcon />} size="medium" {...args} variant="text" />
+      </div>
+      <div style={{
+      display: 'flex',
+      flexDirection: 'row',
+      alignItems: 'flex-start',
+      gap: '32px'
+    }}>
+        <LinkButton fixedIcon={<BlankLinkIcon />} size="small" {...args} />
+        <LinkButton fixedIcon={<BlankLinkIcon />} size="small" {...args} variant="secondary" />
+        <LinkButton fixedIcon={<BlankLinkIcon />} size="small" {...args} variant="accent" />
+        <LinkButton fixedIcon={<BlankLinkIcon />} size="small" {...args} variant="alert" />
+        <LinkButton fixedIcon={<BlankLinkIcon />} size="small" {...args} variant="text" />
+      </div>
+      <div style={{
+      display: 'flex',
+      flexDirection: 'row',
+      alignItems: 'flex-start',
+      gap: '32px'
+    }}>
+        <LinkButton {...args} />
+        <LinkButton {...args} variant="secondary" />
+        <LinkButton {...args} variant="accent" />
+        <LinkButton {...args} variant="alert" />
+        <LinkButton {...args} variant="text" />
+        <LinkButton {...args} variant="textAlert" />
+      </div>
+      <div style={{
+      display: 'flex',
+      flexDirection: 'row',
+      alignItems: 'flex-start',
+      gap: '32px'
+    }}>
+        <LinkButton {...args} size="medium" />
+        <LinkButton {...args} size="medium" variant="secondary" />
+        <LinkButton {...args} size="medium" variant="accent" />
+        <LinkButton {...args} size="medium" variant="alert" />
+        <LinkButton {...args} size="medium" variant="text" />
+        <LinkButton {...args} size="medium" variant="textAlert" />
+      </div>
+      <div style={{
+      display: 'flex',
+      flexDirection: 'row',
+      alignItems: 'flex-start',
+      gap: '32px'
+    }}>
+        <LinkButton {...args} size="small" />
+        <LinkButton {...args} size="small" variant="secondary" />
+        <LinkButton {...args} size="small" variant="accent" />
+        <LinkButton {...args} size="small" variant="alert" />
+        <LinkButton {...args} size="small" variant="text" />
+        <LinkButton {...args} size="small" variant="textAlert" />
+      </div>
+      <div style={{
+      display: 'flex',
+      flexDirection: 'column',
+      alignItems: 'flex-start',
+      gap: '32px'
+    }}>
+        <LinkButton {...args} size="small" block variant="secondary" />
+        <LinkButton {...args} size="medium" block variant="secondary" />
+        <LinkButton {...args} size="large" block variant="secondary" />
+      </div>
+      <div style={{
+      display: 'flex',
+      flexDirection: 'row',
+      alignItems: 'flex-start',
+      gap: '32px'
+    }}>
+        <LinkButton href="https://vitals.ubie.life/" {...args} disabled />
+        <LinkButton href="https://vitals.ubie.life/" {...args} variant="secondary" disabled />
+        <LinkButton href="https://vitals.ubie.life/" {...args} variant="accent" disabled />
+        <LinkButton href="https://vitals.ubie.life/" {...args} variant="alert" disabled />
+        <LinkButton href="https://vitals.ubie.life/" {...args} variant="text" disabled />
+        <LinkButton href="https://vitals.ubie.life/" {...args} variant="textAlert" disabled />
+      </div>
+    </div>,
+  args: defaultArgs
+}`,...(z=(y=o.parameters)==null?void 0:y.docs)==null?void 0:z.source}}};const K=["Default"];export{o as Default,K as __namedExportsOrder,J as default};
+//# sourceMappingURL=LinkButton.stories-805b963f.js.map
---
+++ assets/WithTooltip-J57HCPYA-742ccb34.js
@@ -0,0 +1,2 @@
+import{W as h,W as l,a as s}from"./DocsRenderer-3PUGWF3O-5c05268d.js";import"./iframe-83e1ec45.js";import"../sb-preview/runtime.js";import"./index-76fb7be0.js";import"./_commonjsHelpers-de833af9.js";import"./_getPrototype-725791a0.js";import"./index-d3ea75b5.js";import"./index-d37d4223.js";import"./index-356e4a49.js";export{h as WithToolTipState,l as WithTooltip,s as WithTooltipPure};
+//# sourceMappingURL=WithTooltip-J57HCPYA-742ccb34.js.map
