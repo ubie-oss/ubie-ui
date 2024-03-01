e972ea7e5ed735aec4fa4b5b026fbad1af67b702
kodai3 <k3dai.su3@gmail.com>
2024-03-01 22:22:25 +0900

    feat: 🎸 add Toggle component
    Signed-off-by: kodai3 <k3dai.su3@gmail.com>

---
+++ src/stories/Toggle.stories.tsx
@@ -0,0 +1,17 @@
+import { Meta, StoryObj } from '@storybook/react';
+import { Toggle } from '..';
+
+export default {
+  title: 'Form/Toggle',
+  component: Toggle,
+} as Meta<typeof Toggle>;
+
+type Story = StoryObj<typeof Toggle>;
+
+export const Default: Story = {};
+
+export const DefaultChecked: Story = {
+  args: {
+    defaultChecked: true,
+  },
+};
