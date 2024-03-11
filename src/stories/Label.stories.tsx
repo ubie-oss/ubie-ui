86ae2d4b23695e238139b50073284e27847254c6
8845musign <hiroki.yokouchi@dr-ubie.com>
2024-03-11 19:58:09 +0900

    Do not use <Label> as a legend element; use XXXXGroup component
    Signed-off-by: 8845musign <hiroki.yokouchi@dr-ubie.com>

--- src/stories/Label.stories.tsx
+++ src/stories/Label.stories.tsx
@@ -11,7 +11,3 @@ type Story = StoryObj<typeof Label>;
 export const Default: Story = {
   render: () => <Label htmlFor="id">全角カタカナでご入力ください</Label>,
 };
-
-export const AsLegend: Story = {
-  render: () => <Label as="legend">選択してください（複数回答可）</Label>,
-};
