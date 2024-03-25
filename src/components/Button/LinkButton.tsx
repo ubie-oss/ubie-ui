d8a969f8aebf894a4a1fbf616b5143066b9aec4f
8845musign <hiroki.yokouchi@dr-ubie.com>
2024-03-25 14:42:23 +0900

    delete className prop
    Signed-off-by: 8845musign <hiroki.yokouchi@dr-ubie.com>

--- src/components/Button/LinkButton.tsx
+++ src/components/Button/LinkButton.tsx
@@ -14,7 +14,6 @@ export const LinkButton = forwardRef<HTMLAnchorElement, LinkButtonProps>(
       children,
       variant = 'primary',
       size = 'large',
-      className,
       block = false,
       icon: _icon,
       fixedIcon: _fixedIcon,
@@ -28,16 +27,13 @@ export const LinkButton = forwardRef<HTMLAnchorElement, LinkButtonProps>(
     const icon = _icon === 'default' ? <VariantIcon variant={variant} /> : _icon;
     const fixedIcon = _fixedIcon === 'default' ? <VariantIcon variant={variant} /> : _fixedIcon;
     const suffixIcon = _suffixIcon === 'default' ? <VariantIcon variant={variant} /> : _suffixIcon;
-    const cls = clsx(
-      {
-        [styles.button]: true,
-        [styles[variant]]: true,
-        [styles[size]]: true,
-        [styles.block]: block,
-        [styles.disabled]: disabled,
-      },
-      className,
-    );
+    const cls = clsx({
+      [styles.button]: true,
+      [styles[variant]]: true,
+      [styles[size]]: true,
+      [styles.block]: block,
+      [styles.disabled]: disabled,
+    });
     const href = disabled ? undefined : _href;
 
     /* eslint-disable-next-line @typescript-eslint/no-explicit-any */
