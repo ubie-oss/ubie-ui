61148184038c35aec993edbade8933ca1bbabf3a
8845musign <hiroki.yokouchi@dr-ubie.com>
2024-04-01 10:37:30 +0900

    upgrade & migratie storybook
    Signed-off-by: 8845musign <hiroki.yokouchi@dr-ubie.com>

--- .storybook/preview.js
+++ .storybook/preview.js
@@ -1,7 +1,6 @@
 import '@ubie/design-tokens/dist/tokens.css';
 
 export const parameters = {
-  actions: { argTypesRegex: '^on[A-Z].*' },
   controls: {
     matchers: {
       color: /(background|color)$/i,
