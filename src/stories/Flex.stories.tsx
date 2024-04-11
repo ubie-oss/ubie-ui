fb9175bfef1d56e4db78eddbbbbb6524e43c80d4
8845musign <hiroki.yokouchi@dr-ubie.com>
2024-04-11 17:12:27 +0900

    Add <Flex> Stories
    Signed-off-by: 8845musign <hiroki.yokouchi@dr-ubie.com>

---
+++ src/stories/Flex.stories.tsx
@@ -0,0 +1,126 @@
+import { Meta, StoryObj } from '@storybook/react';
+import { Flex } from '..';
+
+export default {
+  component: Flex,
+} satisfies Meta<typeof Flex>;
+
+type Story = StoryObj<typeof Flex>;
+
+export const Spacing: Story = {
+  render: () => (
+    <div>
+      <Flex>
+        <div>none</div>
+        <div>none</div>
+        <div>none</div>
+      </Flex>
+
+      <Flex spacing="xxs">
+        <div>xxs</div>
+        <div>xxs</div>
+        <div>xxs</div>
+      </Flex>
+
+      <Flex spacing="xs">
+        <div>xs</div>
+        <div>xs</div>
+        <div>xs</div>
+      </Flex>
+
+      <Flex spacing="sm">
+        <div>sm</div>
+        <div>sm</div>
+        <div>sm</div>
+      </Flex>
+
+      <Flex spacing="md">
+        <div>md</div>
+        <div>md</div>
+        <div>md</div>
+      </Flex>
+
+      <Flex spacing="lg">
+        <div>lg</div>
+        <div>lg</div>
+        <div>lg</div>
+      </Flex>
+
+      <Flex spacing="xl">
+        <div>xl</div>
+        <div>xl</div>
+        <div>xl</div>
+      </Flex>
+
+      <Flex spacing="xxl">
+        <div>xxl</div>
+        <div>xxl</div>
+        <div>xxl</div>
+      </Flex>
+    </div>
+  ),
+};
+
+export const Vertical: Story = {
+  render: () => (
+    <div>
+      <Flex direction="column" spacing="md">
+        <p>Item</p>
+        <p>Item</p>
+        <p>Item</p>
+        <p>Item</p>
+      </Flex>
+
+      <p>※特別な理由がなければStackをつかってください</p>
+    </div>
+  ),
+};
+
+export const StretchedChildren: Story = {
+  render: () => (
+    <div style={{ height: 500 }}>
+      <Flex spacing="lg" alignItems="stretch" height="full">
+        <p style={{ margin: 0, background: '#DDD' }}>Stretched</p>
+        <p style={{ margin: 0, background: '#DDD' }}>Stretched</p>
+      </Flex>
+    </div>
+  ),
+};
+
+export const SpaceBetween: Story = {
+  render: () => (
+    <>
+      <Flex justifyContent="space-between">
+        <p>Item</p>
+        <p>Item</p>
+        <p>Item</p>
+        <p>Item</p>
+      </Flex>
+
+      <Flex justifyContent="space-between">
+        <p>Item</p>
+        <p>Item</p>
+      </Flex>
+    </>
+  ),
+};
+
+export const Centralized: Story = {
+  render: () => (
+    <Flex direction="row" justifyContent="center" alignItems="center" spacing="sm">
+      <span>
+        Words
+        <br />
+        crowded
+      </span>
+      <span>in</span>
+      <span>
+        simply
+        <br />
+        dummy
+        <br />
+        text
+      </span>
+    </Flex>
+  ),
+};
