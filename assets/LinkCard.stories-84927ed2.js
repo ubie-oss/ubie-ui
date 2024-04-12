dd444d63a676e35640adb1ec02a37fd12dc06032
takanorip <takanorip@users.noreply.github.com>
2024-04-12 10:35:31 +0000

    deploy: 45a763bf8c74ce1511c5cc51f567a4a67677d531

---
+++ assets/LinkCard.stories-84927ed2.js
@@ -0,0 +1,25 @@
+import{j as u}from"./jsx-runtime-9c4ae004.js";import{c as z}from"./clsx-1229b3e0.js";import{r as f}from"./index-1b03fe98.js";import{S as V}from"./Stack-9aaccb72.js";function N(e){return u.jsx("svg",Object.assign({width:"1em",height:"1em",viewBox:"0 0 24 24",fill:"none",xmlns:"http://www.w3.org/2000/svg"},e,{children:u.jsx("path",{fillRule:"evenodd",clipRule:"evenodd",d:"M16.8839 12.8839C17.372 12.3957 17.372 11.6043 16.8839 11.1161L8.88388 3.11612C8.39573 2.62796 7.60427 2.62796 7.11612 3.11612C6.62796 3.60427 6.62796 4.39573 7.11612 4.88388L14.2322 12L7.11612 19.1161C6.62796 19.6043 6.62796 20.3957 7.11612 20.8839C7.60427 21.372 8.39573 21.372 8.88388 20.8839L16.8839 12.8839Z",fill:"currentColor"},void 0)}),void 0)}function l(e){return u.jsxs("svg",Object.assign({width:"1em",height:"1em",viewBox:"0 0 24 24",fill:"none",xmlns:"http://www.w3.org/2000/svg"},e,{children:[u.jsx("path",{d:"M13 10.5V9H14.5V7H13V5.5L11 5.5V7H9.5V9H11V10.5H13Z",fill:"currentColor"},void 0),u.jsx("path",{fillRule:"evenodd",clipRule:"evenodd",d:"M4 22C2.89543 22 2 21.1046 2 20V9C2 7.89543 2.89543 7 4 7H5.5V4C5.5 2.89543 6.39543 2 7.5 2H16.5C17.6046 2 18.5 2.89543 18.5 4V7H20C21.1046 7 22 7.89543 22 9V20C22 21.1046 21.1046 22 20 22H4ZM4 9H7.5V4L16.5 4V9H20V20H16V14C16 13.4477 15.5523 13 15 13H9C8.44772 13 8 13.4477 8 14V20H4V9ZM10 20H14V15H10V20Z",fill:"currentColor"},void 0)]}),void 0)}const R="_card_pbmrl_1",M="_icon_pbmrl_15",Z="_text_pbmrl_21",D="_title_pbmrl_28",W="_description_pbmrl_35",y="_caret_pbmrl_42",O="_medium_pbmrl_49",q="_small_pbmrl_56",r={card:R,icon:M,text:Z,title:D,description:W,caret:y,medium:O,small:q},s=f.forwardRef(({title:e,size:L="medium",className:k,icon:c,description:d,render:m,...S},w)=>{const I=z(r[L],r.card,k);return((p,C)=>m?f.cloneElement(m,p,C):u.jsx("a",{...p,children:C}))({className:I,...S,ref:w},u.jsxs(u.Fragment,{children:[c&&u.jsx(c,{className:r.icon}),u.jsxs("div",{className:r.text,children:[u.jsx("p",{className:r.title,children:e}),d&&u.jsx("p",{className:r.description,children:d})]}),u.jsx(N,{className:r.caret})]}))});s.displayName="LinkCard";s.__docgenInfo={description:"",methods:[],displayName:"LinkCard",props:{size:{defaultValue:{value:"'medium'",computed:!1},required:!1}}};const T={component:s},o={title:"見出しテキスト"},t={render:e=>u.jsxs(V,{spacing:"md",alignItems:"normal",children:[u.jsx(s,{...e,href:"https://vitals.ubie.life/"}),u.jsx(s,{...e,href:"https://vitals.ubie.life/"})]}),args:o},i={render:e=>u.jsx(s,{...e,href:"https://vitals.ubie.life/",icon:l}),args:o},a={render:e=>u.jsx(s,{...e,href:"https://vitals.ubie.life/",icon:l}),args:{...o,description:"これはこのカードの遷移先の説明です。これはこのカードの遷移先の説明です。これはこのカードの遷移先の説明です。これはこのカードの遷移先の説明です。これはこのカードの遷移先の説明です。"}},n={render:e=>u.jsxs(V,{spacing:"md",alignItems:"normal",children:[u.jsx(s,{...e,href:"https://vitals.ubie.life/",size:"small"}),u.jsx(s,{...e,href:"https://vitals.ubie.life/",icon:l,size:"small"}),u.jsx(s,{...e,href:"https://vitals.ubie.life/",icon:l,description:"これはこのカードの遷移先の説明です。これはこのカードの遷移先の説明です。これはこのカードの遷移先の説明です。これはこのカードの遷移先の説明です。これはこのカードの遷移先の説明です。",size:"small"})]}),args:o};var h,g,E;t.parameters={...t.parameters,docs:{...(h=t.parameters)==null?void 0:h.docs,source:{originalSource:`{
+  render: args => <Stack spacing="md" alignItems="normal">
+      <LinkCard {...args} href="https://vitals.ubie.life/" />
+      <LinkCard {...args} href="https://vitals.ubie.life/" />
+    </Stack>,
+  args: defaultArgs
+}`,...(E=(g=t.parameters)==null?void 0:g.docs)==null?void 0:E.source}}};var A,x,v;i.parameters={...i.parameters,docs:{...(A=i.parameters)==null?void 0:A.docs,source:{originalSource:`{
+  render: args => <LinkCard {...args} href="https://vitals.ubie.life/" icon={HospitalIcon} />,
+  args: defaultArgs
+}`,...(v=(x=i.parameters)==null?void 0:x.docs)==null?void 0:v.source}}};var F,_,j;a.parameters={...a.parameters,docs:{...(F=a.parameters)==null?void 0:F.docs,source:{originalSource:`{
+  render: args => <LinkCard {...args} href="https://vitals.ubie.life/" icon={HospitalIcon} />,
+  args: {
+    ...defaultArgs,
+    description: 'これはこのカードの遷移先の説明です。これはこのカードの遷移先の説明です。これはこのカードの遷移先の説明です。これはこのカードの遷移先の説明です。これはこのカードの遷移先の説明です。'
+  }
+}`,...(j=(_=a.parameters)==null?void 0:_.docs)==null?void 0:j.source}}};var b,B,H;n.parameters={...n.parameters,docs:{...(b=n.parameters)==null?void 0:b.docs,source:{originalSource:`{
+  render: args => {
+    return <Stack spacing="md" alignItems="normal">
+        <LinkCard {...args} href="https://vitals.ubie.life/" size="small" />
+        <LinkCard {...args} href="https://vitals.ubie.life/" icon={HospitalIcon} size="small" />
+        <LinkCard {...args} href="https://vitals.ubie.life/" icon={HospitalIcon} description="これはこのカードの遷移先の説明です。これはこのカードの遷移先の説明です。これはこのカードの遷移先の説明です。これはこのカードの遷移先の説明です。これはこのカードの遷移先の説明です。" size="small" />
+      </Stack>;
+  },
+  args: defaultArgs
+}`,...(H=(B=n.parameters)==null?void 0:B.docs)==null?void 0:H.source}}};const U=["Default","WithIcon","WithDescription","SizeSmall"];export{t as Default,n as SizeSmall,a as WithDescription,i as WithIcon,U as __namedExportsOrder,T as default};
