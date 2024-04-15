a84730869cb9339870bd603dcfce256982953bb8
takanorip <takanorip@users.noreply.github.com>
2024-04-15 10:05:20 +0000

    deploy: e8571d3dacf0e2a79e57bec421dcfd6c30060c7e

---
+++ assets/Box.stories-87f44578.js
@@ -0,0 +1,122 @@
+import{j as r}from"./jsx-runtime-9c4ae004.js";import{B as d}from"./Box-2da007f2.js";import{S as E}from"./Stack-9aaccb72.js";import"./index-1b03fe98.js";import"./clsx-1229b3e0.js";import"./style-6e220354.js";const N={component:d},m={children:"Box",pt:"md",pr:"md",pb:"md",pl:"md",radius:"md",backgroundColor:"primary"},s={args:m},n={render:()=>r.jsxs("div",{children:[r.jsx(d,{...m,backgroundColor:"primary",children:"Primary"}),r.jsx(d,{...m,mt:"md",backgroundColor:"primaryDarken",children:"Primary Darken"}),r.jsx(d,{...m,mt:"md",backgroundColor:"accent",children:"Accent"}),r.jsx(d,{...m,mt:"md",backgroundColor:"accentDarken",children:"Accent Darken"}),r.jsx(d,{...m,mt:"md",backgroundColor:"alert",children:"Alert"}),r.jsx(d,{...m,mt:"md",backgroundColor:"gray",children:"Gray"}),r.jsx(d,{...m,mt:"md",backgroundColor:"white",children:"White"})]})},x={render:()=>r.jsxs("div",{children:[r.jsx(d,{...m,pt:"xxs",pr:"xxs",pb:"xxs",pl:"xxs",children:"xxs"}),r.jsx(d,{...m,mt:"md",pt:"xs",pr:"xs",pb:"xs",pl:"xs",children:"xs"}),r.jsx(d,{...m,mt:"md",pt:"sm",pr:"sm",pb:"sm",pl:"sm",children:"sm"}),r.jsx(d,{...m,mt:"md",pt:"md",pr:"md",pb:"md",pl:"md",children:"md"}),r.jsx(d,{...m,mt:"md",pt:"lg",pr:"lg",pb:"lg",pl:"lg",children:"lg"}),r.jsx(d,{...m,mt:"md",pt:"xl",pr:"xl",pb:"xl",pl:"xl",children:"xl"}),r.jsx(d,{...m,mt:"md",pt:"xxl",pr:"xxl",pb:"xxl",pl:"xxl",children:"xxl"})]})},e={render:()=>r.jsxs("div",{children:[r.jsx(d,{...m,mt:"xxs",mr:"xxs",mb:"xxs",ml:"xxs",children:"xxs"}),r.jsx("hr",{}),r.jsx(d,{...m,mt:"xs",mr:"xs",mb:"xs",ml:"xs",children:"xs"}),r.jsx("hr",{}),r.jsx(d,{...m,mt:"sm",mr:"sm",mb:"sm",ml:"sm",children:"sm"}),r.jsx("hr",{}),r.jsx(d,{...m,mt:"md",mr:"md",mb:"md",ml:"md",children:"md"}),r.jsx("hr",{}),r.jsx(d,{...m,mt:"lg",mr:"lg",mb:"lg",ml:"lg",children:"lg"}),r.jsx("hr",{}),r.jsx(d,{...m,mt:"xl",mr:"xl",mb:"xl",ml:"xl",children:"xl"}),r.jsx("hr",{}),r.jsx(d,{...m,mt:"xxl",mr:"xxl",mb:"xxl",ml:"xxl",children:"xxl"})]})},o={render:()=>r.jsxs("div",{children:[r.jsx(d,{...m,pt:"md",pr:"md",pb:"md",pl:"md",radius:"xs",children:"xs"}),r.jsx(d,{...m,mt:"md",pt:"md",pr:"md",pb:"md",pl:"md",radius:"sm",children:"sm"}),r.jsx(d,{...m,mt:"md",pt:"md",pr:"md",pb:"md",pl:"md",radius:"md",children:"md"}),r.jsx(d,{...m,mt:"md",pt:"md",pr:"md",pb:"md",pl:"md",radius:"lg",children:"lg"})]})},l={render:()=>r.jsxs("div",{children:[r.jsx(d,{...m,backgroundColor:"white",border:"gray",children:"Border Gray"}),r.jsx(d,{mt:"md",...m,backgroundColor:"white",border:"grayThick",children:"Border Gray Thick"}),r.jsx(d,{...m,mt:"md",backgroundColor:"white",border:"primary",children:"Primary Border"}),r.jsx(d,{...m,mt:"md",backgroundColor:"white",border:"primaryThick",children:"Primary Border Thick"})]})},t={render:()=>r.jsxs(E,{spacing:"md",children:[r.jsx("p",{children:"Flexコンテナの子要素では、デフォルトでは横幅がintrinsic（コンテンツに応じた幅）となります。widthをfullとすることで100%を維持できます。"}),r.jsx(d,{...m,width:"full",children:"Width Full"}),r.jsx(d,{...m,children:"Not Width Full"})]})};var a,u,c;s.parameters={...s.parameters,docs:{...(a=s.parameters)==null?void 0:a.docs,source:{originalSource:`{
+  args: defaultArgs
+}`,...(c=(u=s.parameters)==null?void 0:u.docs)==null?void 0:c.source}}};var i,p,g;n.parameters={...n.parameters,docs:{...(i=n.parameters)==null?void 0:i.docs,source:{originalSource:`{
+  render: () => <div>
+      <Box {...defaultArgs} backgroundColor="primary">
+        Primary
+      </Box>
+      <Box {...defaultArgs} mt="md" backgroundColor="primaryDarken">
+        Primary Darken
+      </Box>
+      <Box {...defaultArgs} mt="md" backgroundColor="accent">
+        Accent
+      </Box>
+      <Box {...defaultArgs} mt="md" backgroundColor="accentDarken">
+        Accent Darken
+      </Box>
+      <Box {...defaultArgs} mt="md" backgroundColor="alert">
+        Alert
+      </Box>
+      <Box {...defaultArgs} mt="md" backgroundColor="gray">
+        Gray
+      </Box>
+      <Box {...defaultArgs} mt="md" backgroundColor="white">
+        White
+      </Box>
+    </div>
+}`,...(g=(p=n.parameters)==null?void 0:p.docs)==null?void 0:g.source}}};var B,h,b;x.parameters={...x.parameters,docs:{...(B=x.parameters)==null?void 0:B.docs,source:{originalSource:`{
+  render: () => <div>
+      <Box {...defaultArgs} pt="xxs" pr="xxs" pb="xxs" pl="xxs">
+        xxs
+      </Box>
+      <Box {...defaultArgs} mt="md" pt="xs" pr="xs" pb="xs" pl="xs">
+        xs
+      </Box>
+      <Box {...defaultArgs} mt="md" pt="sm" pr="sm" pb="sm" pl="sm">
+        sm
+      </Box>
+      <Box {...defaultArgs} mt="md" pt="md" pr="md" pb="md" pl="md">
+        md
+      </Box>
+      <Box {...defaultArgs} mt="md" pt="lg" pr="lg" pb="lg" pl="lg">
+        lg
+      </Box>
+      <Box {...defaultArgs} mt="md" pt="xl" pr="xl" pb="xl" pl="xl">
+        xl
+      </Box>
+      <Box {...defaultArgs} mt="md" pt="xxl" pr="xxl" pb="xxl" pl="xxl">
+        xxl
+      </Box>
+    </div>
+}`,...(b=(h=x.parameters)==null?void 0:h.docs)==null?void 0:b.source}}};var j,A,k;e.parameters={...e.parameters,docs:{...(j=e.parameters)==null?void 0:j.docs,source:{originalSource:`{
+  render: () => <div>
+      <Box {...defaultArgs} mt="xxs" mr="xxs" mb="xxs" ml="xxs">
+        xxs
+      </Box>
+      <hr />
+      <Box {...defaultArgs} mt="xs" mr="xs" mb="xs" ml="xs">
+        xs
+      </Box>
+      <hr />
+      <Box {...defaultArgs} mt="sm" mr="sm" mb="sm" ml="sm">
+        sm
+      </Box>
+      <hr />
+      <Box {...defaultArgs} mt="md" mr="md" mb="md" ml="md">
+        md
+      </Box>
+      <hr />
+      <Box {...defaultArgs} mt="lg" mr="lg" mb="lg" ml="lg">
+        lg
+      </Box>
+      <hr />
+      <Box {...defaultArgs} mt="xl" mr="xl" mb="xl" ml="xl">
+        xl
+      </Box>
+      <hr />
+      <Box {...defaultArgs} mt="xxl" mr="xxl" mb="xxl" ml="xxl">
+        xxl
+      </Box>
+    </div>
+}`,...(k=(A=e.parameters)==null?void 0:A.docs)==null?void 0:k.source}}};var f,C,y;o.parameters={...o.parameters,docs:{...(f=o.parameters)==null?void 0:f.docs,source:{originalSource:`{
+  render: () => <div>
+      <Box {...defaultArgs} pt="md" pr="md" pb="md" pl="md" radius="xs">
+        xs
+      </Box>
+      <Box {...defaultArgs} mt="md" pt="md" pr="md" pb="md" pl="md" radius="sm">
+        sm
+      </Box>
+      <Box {...defaultArgs} mt="md" pt="md" pr="md" pb="md" pl="md" radius="md">
+        md
+      </Box>
+      <Box {...defaultArgs} mt="md" pt="md" pr="md" pb="md" pl="md" radius="lg">
+        lg
+      </Box>
+    </div>
+}`,...(y=(C=o.parameters)==null?void 0:C.docs)==null?void 0:y.source}}};var F,D,v;l.parameters={...l.parameters,docs:{...(F=l.parameters)==null?void 0:F.docs,source:{originalSource:`{
+  render: () => <div>
+      <Box {...defaultArgs} backgroundColor="white" border="gray">
+        Border Gray
+      </Box>
+      <Box mt="md" {...defaultArgs} backgroundColor="white" border="grayThick">
+        Border Gray Thick
+      </Box>
+      <Box {...defaultArgs} mt="md" backgroundColor="white" border="primary">
+        Primary Border
+      </Box>
+      <Box {...defaultArgs} mt="md" backgroundColor="white" border="primaryThick">
+        Primary Border Thick
+      </Box>
+    </div>
+}`,...(v=(D=l.parameters)==null?void 0:D.docs)==null?void 0:v.source}}};var w,S,P;t.parameters={...t.parameters,docs:{...(w=t.parameters)==null?void 0:w.docs,source:{originalSource:`{
+  render: () => <Stack spacing="md">
+      <p>
+        Flexコンテナの子要素では、デフォルトでは横幅がintrinsic（コンテンツに応じた幅）となります。widthをfullとすることで100%を維持できます。
+      </p>
+      <Box {...defaultArgs} width="full">
+        Width Full
+      </Box>
+
+      <Box {...defaultArgs}>Not Width Full</Box>
+    </Stack>
+}`,...(P=(S=t.parameters)==null?void 0:S.docs)==null?void 0:P.source}}};const O=["Default","BackgroundColor","Padding","Margin","Radius","Border","Width"];export{n as BackgroundColor,l as Border,s as Default,e as Margin,x as Padding,o as Radius,t as Width,O as __namedExportsOrder,N as default};
---
+++ assets/WithTooltip-Y7J54OF7-56946338.js
@@ -0,0 +1,1 @@
+import{W as h,W as l,b as s}from"./DocsRenderer-K4EAMTCU-cf4c5772.js";import"./iframe-b2244b10.js";import"../sb-preview/runtime.js";import"./index-1b03fe98.js";import"./react-18-5df836b6.js";import"./index-6fd5a17b.js";import"./index-d8983a70.js";import"./_getPrototype-dd248945.js";import"./index-356e4a49.js";export{h as WithToolTipState,l as WithTooltip,s as WithTooltipPure};
