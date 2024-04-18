0badb1c207de15fd552296681f84d9aeee85758e
takanorip <takanorip@users.noreply.github.com>
2024-04-18 06:07:13 +0000

    deploy: 04e4a1cdf90cbd28b0db488404e445d57597a6bd

---
+++ assets/Box.stories-7f113723.js
@@ -0,0 +1,128 @@
+import{j as r}from"./jsx-runtime-9c4ae004.js";import{B as d}from"./Box-633c7dea.js";import{S as R}from"./Stack-51d5a3f7.js";import"./index-1b03fe98.js";import"./clsx-1229b3e0.js";import"./style-6e220354.js";const z={component:d},m={children:"Box",pt:"md",pr:"md",pb:"md",pl:"md",radius:"md",backgroundColor:"primary"},s={args:m},n={render:()=>r.jsxs("div",{children:[r.jsx(d,{...m,backgroundColor:"primary",children:"Primary"}),r.jsx(d,{...m,mt:"md",backgroundColor:"primaryDarken",children:"Primary Darken"}),r.jsx(d,{...m,mt:"md",backgroundColor:"accent",children:"Accent"}),r.jsx(d,{...m,mt:"md",backgroundColor:"accentDarken",children:"Accent Darken"}),r.jsx(d,{...m,mt:"md",backgroundColor:"alert",children:"Alert"}),r.jsx(d,{...m,mt:"md",backgroundColor:"gray",children:"Gray"}),r.jsx(d,{...m,mt:"md",backgroundColor:"white",children:"White"})]})},e={render:()=>r.jsxs("div",{children:[r.jsx(d,{...m,pt:"xxs",pr:"xxs",pb:"xxs",pl:"xxs",children:"xxs"}),r.jsx(d,{...m,mt:"md",pt:"xs",pr:"xs",pb:"xs",pl:"xs",children:"xs"}),r.jsx(d,{...m,mt:"md",pt:"sm",pr:"sm",pb:"sm",pl:"sm",children:"sm"}),r.jsx(d,{...m,mt:"md",pt:"md",pr:"md",pb:"md",pl:"md",children:"md"}),r.jsx(d,{...m,mt:"md",pt:"lg",pr:"lg",pb:"lg",pl:"lg",children:"lg"}),r.jsx(d,{...m,mt:"md",pt:"xl",pr:"xl",pb:"xl",pl:"xl",children:"xl"}),r.jsx(d,{...m,mt:"md",pt:"xxl",pr:"xxl",pb:"xxl",pl:"xxl",children:"xxl"})]})},o={render:()=>r.jsxs("div",{children:[r.jsx(d,{...m,mt:"xxs",mr:"xxs",mb:"xxs",ml:"xxs",children:"xxs"}),r.jsx("hr",{}),r.jsx(d,{...m,mt:"xs",mr:"xs",mb:"xs",ml:"xs",children:"xs"}),r.jsx("hr",{}),r.jsx(d,{...m,mt:"sm",mr:"sm",mb:"sm",ml:"sm",children:"sm"}),r.jsx("hr",{}),r.jsx(d,{...m,mt:"md",mr:"md",mb:"md",ml:"md",children:"md"}),r.jsx("hr",{}),r.jsx(d,{...m,mt:"lg",mr:"lg",mb:"lg",ml:"lg",children:"lg"}),r.jsx("hr",{}),r.jsx(d,{...m,mt:"xl",mr:"xl",mb:"xl",ml:"xl",children:"xl"}),r.jsx("hr",{}),r.jsx(d,{...m,mt:"xxl",mr:"xxl",mb:"xxl",ml:"xxl",children:"xxl"})]})},x={render:()=>r.jsxs("div",{children:[r.jsx(d,{...m,pt:"md",pr:"md",pb:"md",pl:"md",radius:"xs",children:"xs"}),r.jsx(d,{...m,mt:"md",pt:"md",pr:"md",pb:"md",pl:"md",radius:"sm",children:"sm"}),r.jsx(d,{...m,mt:"md",pt:"md",pr:"md",pb:"md",pl:"md",radius:"md",children:"md"}),r.jsx(d,{...m,mt:"md",pt:"md",pr:"md",pb:"md",pl:"md",radius:"lg",children:"lg"})]})},l={render:()=>r.jsxs("div",{children:[r.jsx(d,{...m,backgroundColor:"white",border:"gray",children:"Border Gray"}),r.jsx(d,{mt:"md",...m,backgroundColor:"white",border:"grayThick",children:"Border Gray Thick"}),r.jsx(d,{...m,mt:"md",backgroundColor:"white",border:"primary",children:"Primary Border"}),r.jsx(d,{...m,mt:"md",backgroundColor:"white",border:"primaryThick",children:"Primary Border Thick"})]})},a={render:()=>r.jsxs(R,{spacing:"md",children:[r.jsx("p",{children:"Flexコンテナの子要素では、デフォルトでは横幅がintrinsic（コンテンツに応じた幅）となります。widthをfullとすることで100%を維持できます。"}),r.jsx(d,{...m,width:"full",children:"Width Full"}),r.jsx(d,{...m,children:"Not Width Full"})]})},t={render:()=>r.jsxs(d,{as:"section",pt:"md",pr:"md",pb:"md",pl:"md",radius:"md",backgroundColor:"primary",children:[r.jsx("h2",{children:"Heading"}),r.jsx("p",{children:"body"})]})};var c,u,i;s.parameters={...s.parameters,docs:{...(c=s.parameters)==null?void 0:c.docs,source:{originalSource:`{
+  args: defaultArgs
+}`,...(i=(u=s.parameters)==null?void 0:u.docs)==null?void 0:i.source}}};var p,g,h;n.parameters={...n.parameters,docs:{...(p=n.parameters)==null?void 0:p.docs,source:{originalSource:`{
+  render: () => <div>
+      <Box {...defaultArgs} backgroundColor="primary">
+        Primary
+      </Box>
+      <Box {...defaultArgs} mt="md" backgroundColor="primaryDarken">
+        Primary Darken
+      </Box>
+      <Box {...defaultArgs} mt="md" backgroundColor="accent">
+        Accent
+      </Box>
+      <Box {...defaultArgs} mt="md" backgroundColor="accentDarken">
+        Accent Darken
+      </Box>
+      <Box {...defaultArgs} mt="md" backgroundColor="alert">
+        Alert
+      </Box>
+      <Box {...defaultArgs} mt="md" backgroundColor="gray">
+        Gray
+      </Box>
+      <Box {...defaultArgs} mt="md" backgroundColor="white">
+        White
+      </Box>
+    </div>
+}`,...(h=(g=n.parameters)==null?void 0:g.docs)==null?void 0:h.source}}};var B,b,j;e.parameters={...e.parameters,docs:{...(B=e.parameters)==null?void 0:B.docs,source:{originalSource:`{
+  render: () => <div>
+      <Box {...defaultArgs} pt="xxs" pr="xxs" pb="xxs" pl="xxs">
+        xxs
+      </Box>
+      <Box {...defaultArgs} mt="md" pt="xs" pr="xs" pb="xs" pl="xs">
+        xs
+      </Box>
+      <Box {...defaultArgs} mt="md" pt="sm" pr="sm" pb="sm" pl="sm">
+        sm
+      </Box>
+      <Box {...defaultArgs} mt="md" pt="md" pr="md" pb="md" pl="md">
+        md
+      </Box>
+      <Box {...defaultArgs} mt="md" pt="lg" pr="lg" pb="lg" pl="lg">
+        lg
+      </Box>
+      <Box {...defaultArgs} mt="md" pt="xl" pr="xl" pb="xl" pl="xl">
+        xl
+      </Box>
+      <Box {...defaultArgs} mt="md" pt="xxl" pr="xxl" pb="xxl" pl="xxl">
+        xxl
+      </Box>
+    </div>
+}`,...(j=(b=e.parameters)==null?void 0:b.docs)==null?void 0:j.source}}};var A,k,f;o.parameters={...o.parameters,docs:{...(A=o.parameters)==null?void 0:A.docs,source:{originalSource:`{
+  render: () => <div>
+      <Box {...defaultArgs} mt="xxs" mr="xxs" mb="xxs" ml="xxs">
+        xxs
+      </Box>
+      <hr />
+      <Box {...defaultArgs} mt="xs" mr="xs" mb="xs" ml="xs">
+        xs
+      </Box>
+      <hr />
+      <Box {...defaultArgs} mt="sm" mr="sm" mb="sm" ml="sm">
+        sm
+      </Box>
+      <hr />
+      <Box {...defaultArgs} mt="md" mr="md" mb="md" ml="md">
+        md
+      </Box>
+      <hr />
+      <Box {...defaultArgs} mt="lg" mr="lg" mb="lg" ml="lg">
+        lg
+      </Box>
+      <hr />
+      <Box {...defaultArgs} mt="xl" mr="xl" mb="xl" ml="xl">
+        xl
+      </Box>
+      <hr />
+      <Box {...defaultArgs} mt="xxl" mr="xxl" mb="xxl" ml="xxl">
+        xxl
+      </Box>
+    </div>
+}`,...(f=(k=o.parameters)==null?void 0:k.docs)==null?void 0:f.source}}};var C,y,F;x.parameters={...x.parameters,docs:{...(C=x.parameters)==null?void 0:C.docs,source:{originalSource:`{
+  render: () => <div>
+      <Box {...defaultArgs} pt="md" pr="md" pb="md" pl="md" radius="xs">
+        xs
+      </Box>
+      <Box {...defaultArgs} mt="md" pt="md" pr="md" pb="md" pl="md" radius="sm">
+        sm
+      </Box>
+      <Box {...defaultArgs} mt="md" pt="md" pr="md" pb="md" pl="md" radius="md">
+        md
+      </Box>
+      <Box {...defaultArgs} mt="md" pt="md" pr="md" pb="md" pl="md" radius="lg">
+        lg
+      </Box>
+    </div>
+}`,...(F=(y=x.parameters)==null?void 0:y.docs)==null?void 0:F.source}}};var D,v,w;l.parameters={...l.parameters,docs:{...(D=l.parameters)==null?void 0:D.docs,source:{originalSource:`{
+  render: () => <div>
+      <Box {...defaultArgs} backgroundColor="white" border="gray">
+        Border Gray
+      </Box>
+      <Box mt="md" {...defaultArgs} backgroundColor="white" border="grayThick">
+        Border Gray Thick
+      </Box>
+      <Box {...defaultArgs} mt="md" backgroundColor="white" border="primary">
+        Primary Border
+      </Box>
+      <Box {...defaultArgs} mt="md" backgroundColor="white" border="primaryThick">
+        Primary Border Thick
+      </Box>
+    </div>
+}`,...(w=(v=l.parameters)==null?void 0:v.docs)==null?void 0:w.source}}};var S,P,E;a.parameters={...a.parameters,docs:{...(S=a.parameters)==null?void 0:S.docs,source:{originalSource:`{
+  render: () => <Stack spacing="md">
+      <p>
+        Flexコンテナの子要素では、デフォルトでは横幅がintrinsic（コンテンツに応じた幅）となります。widthをfullとすることで100%を維持できます。
+      </p>
+      <Box {...defaultArgs} width="full">
+        Width Full
+      </Box>
+
+      <Box {...defaultArgs}>Not Width Full</Box>
+    </Stack>
+}`,...(E=(P=a.parameters)==null?void 0:P.docs)==null?void 0:E.source}}};var T,W,G;t.parameters={...t.parameters,docs:{...(T=t.parameters)==null?void 0:T.docs,source:{originalSource:`{
+  render: () => <Box as="section" pt="md" pr="md" pb="md" pl="md" radius="md" backgroundColor="primary">
+      <h2>Heading</h2>
+
+      <p>body</p>
+    </Box>
+}`,...(G=(W=t.parameters)==null?void 0:W.docs)==null?void 0:G.source}}};const I=["Default","BackgroundColor","Padding","Margin","Radius","Border","Width","AsSection"];export{t as AsSection,n as BackgroundColor,l as Border,s as Default,o as Margin,e as Padding,x as Radius,a as Width,I as __namedExportsOrder,z as default};
