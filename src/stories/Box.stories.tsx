455651201b11f25cd886e5a64f9b9a9a1491e5a8
8845musign <hiroki.yokouchi@dr-ubie.com>
2024-04-26 11:48:04 +0900

    Add  Story of Box Component
    Signed-off-by: 8845musign <hiroki.yokouchi@dr-ubie.com>

--- src/stories/Box.stories.tsx
+++ src/stories/Box.stories.tsx
@@ -137,6 +137,10 @@ export const Border: Story = {
       <Box {...defaultArgs} backgroundColor="white" border="gray">
         Border Gray
       </Box>
+
+      <Box {...defaultArgs} backgroundColor="white" border="gray">
+        Border Gray
+      </Box>
       <Box mt="md" {...defaultArgs} backgroundColor="white" border="grayThick">
         Border Gray Thick
       </Box>
@@ -172,5 +176,119 @@ export const AsSection: Story = {
 
       <p>body</p>
     </Box>
+  ),
+};
+
+export const TextVariations: Story = {
+  render: () => (
+    <div>
+      <Box
+        {...defaultArgs}
+        backgroundColor="gray"
+        textColor="primary"
+        textBold
+        textType="body"
+        textSize="lg"
+        textLeading="narrow"
+      >
+        <p>Text Bold</p>
+      </Box>
+
+      <Box {...defaultArgs} mt="lg" backgroundColor="gray" textColor="main" textBold>
+        <p>Text Bold</p>
+      </Box>
+
+      <Box {...defaultArgs} backgroundColor="gray" mt="xl" textColor="main">
+        <p>Color Main</p>
+      </Box>
+      <Box {...defaultArgs} backgroundColor="gray" mt="md" textColor="sub">
+        <p>Color Sub</p>
+      </Box>
+      <Box {...defaultArgs} backgroundColor="gray" mt="md" textColor="link">
+        <p>Color Main</p>
+      </Box>
+      <Box {...defaultArgs} backgroundColor="gray" mt="md" textColor="linkSub">
+        <p>Color Link Sub</p>
+      </Box>
+      <Box {...defaultArgs} backgroundColor="gray" mt="md" textColor="disabled">
+        <p>Color Disabled</p>
+      </Box>
+      <Box {...defaultArgs} backgroundColor="gray" mt="md" textColor="primary">
+        <p>Color Primary</p>
+      </Box>
+      <Box {...defaultArgs} backgroundColor="gray" mt="md" textColor="accent">
+        <p>Color Accent</p>
+      </Box>
+      <Box {...defaultArgs} backgroundColor="gray" mt="md" textColor="alert">
+        <p>Color Alert</p>
+      </Box>
+      <Box {...defaultArgs} mt="xl" textType="body">
+        <p>Body size & leading default value</p>
+      </Box>
+
+      <Box {...defaultArgs} mt="xl" textType="body" textSize="sm" textLeading="default">
+        <p>Body Small Default</p>
+      </Box>
+      <Box {...defaultArgs} mt="md" textType="body" textSize="sm" textLeading="narrow">
+        <p>Body Small Narrow</p>
+      </Box>
+      <Box {...defaultArgs} mt="md" textType="body" textSize="sm" textLeading="tight">
+        <p>Body Small Tight</p>
+      </Box>
+
+      <Box {...defaultArgs} mt="xl" textType="body" textSize="md" textLeading="default">
+        <p>Body Medium Default</p>
+      </Box>
+      <Box {...defaultArgs} mt="md" textType="body" textSize="md" textLeading="narrow">
+        <p>Body Medium Narrow</p>
+      </Box>
+      <Box {...defaultArgs} mt="md" textType="body" textSize="md" textLeading="tight">
+        <p>Body Medium Tight</p>
+      </Box>
+
+      <Box {...defaultArgs} mt="xl" textType="body" textSize="lg" textLeading="default">
+        <p>Body Large Default</p>
+      </Box>
+      <Box {...defaultArgs} mt="md" textType="body" textSize="lg" textLeading="narrow">
+        <p>Body Large Narrow</p>
+      </Box>
+      <Box {...defaultArgs} mt="md" textType="body" textSize="lg" textLeading="tight">
+        <p>Body Large Tight</p>
+      </Box>
+
+      <Box {...defaultArgs} mt="xl" textType="note">
+        <p>Note size & leading default value</p>
+      </Box>
+
+      <Box {...defaultArgs} mt="xl" textType="note" textSize="sm" textLeading="default">
+        <p>Note Small Default</p>
+      </Box>
+      <Box {...defaultArgs} mt="md" textType="note" textSize="sm" textLeading="narrow">
+        <p>Note Small Narrow</p>
+      </Box>
+      <Box {...defaultArgs} mt="md" textType="note" textSize="sm" textLeading="tight">
+        <p>Note Small Tight</p>
+      </Box>
+
+      <Box {...defaultArgs} mt="xl" textType="note" textSize="md" textLeading="default">
+        <p>Note Medium Default</p>
+      </Box>
+      <Box {...defaultArgs} mt="md" textType="note" textSize="md" textLeading="narrow">
+        <p>Note Medium Narrow</p>
+      </Box>
+      <Box {...defaultArgs} mt="md" textType="note" textSize="md" textLeading="tight">
+        <p>Note Medium Tight</p>
+      </Box>
+
+      <Box {...defaultArgs} mt="xl" textType="note" textSize="lg" textLeading="default">
+        <p>Note Large Default</p>
+      </Box>
+      <Box {...defaultArgs} mt="md" textType="note" textSize="lg" textLeading="narrow">
+        <p>Note Large Narrow</p>
+      </Box>
+      <Box {...defaultArgs} mt="md" textType="note" textSize="lg" textLeading="tight">
+        <p>Note Large Tight</p>
+      </Box>
+    </div>
   ),
 };
