e68eaf964e4a9c6b544fe5dd2d5014516b36025a
hiroki.yokouchi <hiroki.yokouchi@dr-ubie.com>
2024-05-09 13:30:07 +0900

    add test
    Signed-off-by: hiroki.yokouchi <hiroki.yokouchi@dr-ubie.com>

--- src/utils/style.spec.ts
+++ src/utils/style.spec.ts
@@ -1,5 +1,5 @@
 import { describe, it, expect } from 'vitest';
-import { opacityToClassName } from './style';
+import { opacityToClassName, colorVariable } from './style';
 
 describe('opacityToClassName', () => {
   it('shuold return normalOverlay when opacity is normal', () => {
@@ -10,3 +10,37 @@ describe('opacityToClassName', () => {
     expect(opacityToClassName('darker')).toBe('darkerOverlay');
   });
 });
+
+
+describe('craeteTextColorVariable', () => {
+  it('should return main text color variable', () =>{
+    expect(colorVariable('main')).toStrictEqual({
+      '--text-color': 'var(--color-text-main)'
+    })
+  })
+  it('should return sub text color variable', () =>{
+    expect(colorVariable('sub')).toStrictEqual({
+      '--text-color': 'var(--color-text-sub)'
+    })
+  })
+  it('should return primary color variable', () =>{
+    expect(colorVariable('primary')).toStrictEqual({
+      '--text-color': 'var(--color-primary)'
+    })
+  })
+  it('should return accent color variable', () =>{
+    expect(colorVariable('accent')).toStrictEqual({
+      '--text-color': 'var(--color-accent)'
+    })
+  })
+  it('should return alert color variable', () =>{
+    expect(colorVariable('alert')).toStrictEqual({
+      '--text-color': 'var(--color-alert)'
+    })
+  })
+  it('should return link sub text color variable', () =>{
+    expect(colorVariable('linkSub')).toStrictEqual({
+      '--text-color': 'var(--color-text-link-sub)'
+    })
+  })
+})
