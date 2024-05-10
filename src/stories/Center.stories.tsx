8ea9e1297b0a05452bc640f4f158ec81a88346df
hiroki.yokouchi <hiroki.yokouchi@dr-ubie.com>
2024-05-10 12:59:28 +0900

    fix prop
    Signed-off-by: hiroki.yokouchi <hiroki.yokouchi@dr-ubie.com>

--- src/stories/Center.stories.tsx
+++ src/stories/Center.stories.tsx
@@ -64,7 +64,7 @@ export const MarginAndPadding: Story = {
     <div style={{ backgroundColor: 'var(--color-background-primary)', overflow: 'hidden' }}>
       <div>prev element</div>
 
-      <Center mt="lg" mr="lg" mb="lg" ml="lg" pt="xxl" pr="xxl" pb="xxl" pl="xxl" maxWidth="400px">
+      <Center mt="lg" mb="lg" pt="xxl" pr="xxl" pb="xxl" pl="xxl" maxWidth="400px">
         <div style={{ width: '100%', backgroundColor: 'var(--color-background-accent-darken)', overflow: 'hidden' }}>
           <h2>Heading</h2>
           <p>body</p>
