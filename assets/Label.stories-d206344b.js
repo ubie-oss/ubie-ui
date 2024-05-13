b0416b2ff0385b316a3a37cc6adecbbecd3254fe
takanorip <takanorip@users.noreply.github.com>
2024-05-13 04:22:30 +0000

    deploy: 58016e20aca09f8b6a5a542158a0556dc3106fe5

--- assets/Label.stories-d206344b.js
+++
@@ -1,3 +0,0 @@
-import{j as s}from"./jsx-runtime-9c4ae004.js";import{L as a}from"./Label-2710692b.js";import"./index-1b03fe98.js";const n={title:"Form/Label",component:a},e={render:()=>s.jsx(a,{htmlFor:"id",children:"全角カタカナでご入力ください"})};var r,o,t;e.parameters={...e.parameters,docs:{...(r=e.parameters)==null?void 0:r.docs,source:{originalSource:`{
-  render: () => <Label htmlFor="id">全角カタカナでご入力ください</Label>
-}`,...(t=(o=e.parameters)==null?void 0:o.docs)==null?void 0:t.source}}};const c=["Default"];export{e as Default,c as __namedExportsOrder,n as default};
