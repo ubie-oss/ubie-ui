2c0ebac2a088271ed68a44fe3d2b732c5a17ac01
takanorip <takanorip@users.noreply.github.com>
2024-05-16 08:43:07 +0000

    deploy: ae8eea0dc93fa82f9bb68161e27ebec0c30b47e4

---
+++ assets/Box.stories-0c5949f9.js
@@ -0,0 +1,261 @@
+import{j as e}from"./jsx-runtime-9c4ae004.js";import{B as r}from"./Box-3dae8d2b.js";import{S as _}from"./Stack-6136d9d8.js";import"./index-1b03fe98.js";import"./clsx-1229b3e0.js";import"./style-d1e19ac4.js";const K={component:r},n={children:"Box",pt:"md",pr:"md",pb:"md",pl:"md",radius:"md",backgroundColor:"primary"},d={args:n},o={render:t=>e.jsxs("div",{children:[e.jsx(r,{...t,backgroundColor:"primary",children:"Primary"}),e.jsx(r,{...t,mt:"md",backgroundColor:"primaryDarken",children:"Primary Darken"}),e.jsx(r,{...t,mt:"md",backgroundColor:"accent",children:"Accent"}),e.jsx(r,{...t,mt:"md",backgroundColor:"accentDarken",children:"Accent Darken"}),e.jsx(r,{...t,mt:"md",backgroundColor:"alert",children:"Alert"}),e.jsx(r,{...t,mt:"md",backgroundColor:"gray",children:"Gray"}),e.jsx(r,{...t,mt:"md",backgroundColor:"white",children:"White"})]}),args:n},x={render:t=>e.jsxs("div",{children:[e.jsx(r,{...t,pt:"xxs",pr:"xxs",pb:"xxs",pl:"xxs",children:"xxs"}),e.jsx(r,{...t,mt:"md",pt:"xs",pr:"xs",pb:"xs",pl:"xs",children:"xs"}),e.jsx(r,{...t,mt:"md",pt:"sm",pr:"sm",pb:"sm",pl:"sm",children:"sm"}),e.jsx(r,{...t,mt:"md",pt:"md",pr:"md",pb:"md",pl:"md",children:"md"}),e.jsx(r,{...t,mt:"md",pt:"lg",pr:"lg",pb:"lg",pl:"lg",children:"lg"}),e.jsx(r,{...t,mt:"md",pt:"xl",pr:"xl",pb:"xl",pl:"xl",children:"xl"}),e.jsx(r,{...t,mt:"md",pt:"xxl",pr:"xxl",pb:"xxl",pl:"xxl",children:"xxl"})]}),args:n},l={render:t=>e.jsxs("div",{children:[e.jsx(r,{...t,mt:"xxs",mr:"xxs",mb:"xxs",ml:"xxs",children:"xxs"}),e.jsx("hr",{}),e.jsx(r,{...t,mt:"xs",mr:"xs",mb:"xs",ml:"xs",children:"xs"}),e.jsx("hr",{}),e.jsx(r,{...t,mt:"sm",mr:"sm",mb:"sm",ml:"sm",children:"sm"}),e.jsx("hr",{}),e.jsx(r,{...t,mt:"md",mr:"md",mb:"md",ml:"md",children:"md"}),e.jsx("hr",{}),e.jsx(r,{...t,mt:"lg",mr:"lg",mb:"lg",ml:"lg",children:"lg"}),e.jsx("hr",{}),e.jsx(r,{...t,mt:"xl",mr:"xl",mb:"xl",ml:"xl",children:"xl"}),e.jsx("hr",{}),e.jsx(r,{...t,mt:"xxl",mr:"xxl",mb:"xxl",ml:"xxl",children:"xxl"})]}),args:n},m={render:t=>e.jsxs("div",{children:[e.jsx(r,{...t,pt:"md",pr:"md",pb:"md",pl:"md",radius:"xs",children:"xs"}),e.jsx(r,{...t,mt:"md",pt:"md",pr:"md",pb:"md",pl:"md",radius:"sm",children:"sm"}),e.jsx(r,{...t,mt:"md",pt:"md",pr:"md",pb:"md",pl:"md",radius:"md",children:"md"}),e.jsx(r,{...t,mt:"md",pt:"md",pr:"md",pb:"md",pl:"md",radius:"lg",children:"lg"})]}),args:n},a={render:t=>e.jsxs("div",{children:[e.jsx(r,{...t,backgroundColor:"white",border:"gray",children:"Border Gray"}),e.jsx(r,{...t,backgroundColor:"white",border:"gray",children:"Border Gray"}),e.jsx(r,{mt:"md",...t,backgroundColor:"white",border:"grayThick",children:"Border Gray Thick"}),e.jsx(r,{...t,mt:"md",backgroundColor:"white",border:"primary",children:"Primary Border"}),e.jsx(r,{...t,mt:"md",backgroundColor:"white",border:"primaryThick",children:"Primary Border Thick"})]}),args:n},i={render:()=>e.jsxs(_,{spacing:"md",children:[e.jsx("p",{children:"Flexコンテナの子要素では、デフォルトでは横幅がintrinsic（コンテンツに応じた幅）となります。widthをfullとすることで100%を維持できます。"}),e.jsx(r,{...n,width:"full",children:"Width Full"}),e.jsx(r,{...n,children:"Not Width Full"})]})},s={render:()=>e.jsxs(r,{as:"section",pt:"md",pr:"md",pb:"md",pl:"md",radius:"md",backgroundColor:"primary",children:[e.jsx("h2",{children:"Heading"}),e.jsx("p",{children:"body"})]})},p={render:t=>e.jsxs("div",{children:[e.jsxs(r,{...t,backgroundColor:"gray",textColor:"primary",textBold:!0,textType:"body",textSize:"lg",textLeading:"narrow",children:[e.jsx("p",{children:"Text Bold"}),e.jsx(r,{...t,backgroundColor:"gray",children:e.jsx("p",{children:"nested"})}),e.jsx(r,{...t,mt:"md",backgroundColor:"gray",textType:"note",textSize:"lg",textLeading:"tight",textBold:!1,children:e.jsx("p",{children:"nested. reset styles"})})]}),e.jsx(r,{...t,backgroundColor:"gray",mt:"xl",textColor:"main",children:e.jsx("p",{children:"Color Main"})}),e.jsx(r,{...t,backgroundColor:"gray",mt:"md",textColor:"sub",children:e.jsx("p",{children:"Color Sub"})}),e.jsx(r,{...t,backgroundColor:"gray",mt:"md",textColor:"link",children:e.jsx("p",{children:"Color Main"})}),e.jsx(r,{...t,backgroundColor:"gray",mt:"md",textColor:"linkSub",children:e.jsx("p",{children:"Color Link Sub"})}),e.jsx(r,{...t,backgroundColor:"gray",mt:"md",textColor:"disabled",children:e.jsx("p",{children:"Color Disabled"})}),e.jsx(r,{...t,backgroundColor:"gray",mt:"md",textColor:"primary",children:e.jsx("p",{children:"Color Primary"})}),e.jsx(r,{...t,backgroundColor:"gray",mt:"md",textColor:"accent",children:e.jsx("p",{children:"Color Accent"})}),e.jsx(r,{...t,backgroundColor:"gray",mt:"md",textColor:"alert",children:e.jsx("p",{children:"Color Alert"})}),e.jsx(r,{...t,mt:"xl",textType:"body",children:e.jsx("p",{children:"Body size & leading default value"})}),e.jsx(r,{...t,mt:"xl",textType:"body",textSize:"sm",textLeading:"default",children:e.jsx("p",{children:"Body Small Default"})}),e.jsx(r,{...t,mt:"md",textType:"body",textSize:"sm",textLeading:"narrow",children:e.jsx("p",{children:"Body Small Narrow"})}),e.jsx(r,{...t,mt:"md",textType:"body",textSize:"sm",textLeading:"tight",children:e.jsx("p",{children:"Body Small Tight"})}),e.jsx(r,{...t,mt:"xl",textType:"body",textSize:"md",textLeading:"default",children:e.jsx("p",{children:"Body Medium Default"})}),e.jsx(r,{...t,mt:"md",textType:"body",textSize:"md",textLeading:"narrow",children:e.jsx("p",{children:"Body Medium Narrow"})}),e.jsx(r,{...t,mt:"md",textType:"body",textSize:"md",textLeading:"tight",children:e.jsx("p",{children:"Body Medium Tight"})}),e.jsx(r,{...t,mt:"xl",textType:"body",textSize:"lg",textLeading:"default",children:e.jsx("p",{children:"Body Large Default"})}),e.jsx(r,{...t,mt:"md",textType:"body",textSize:"lg",textLeading:"narrow",children:e.jsx("p",{children:"Body Large Narrow"})}),e.jsx(r,{...t,mt:"md",textType:"body",textSize:"lg",textLeading:"tight",children:e.jsx("p",{children:"Body Large Tight"})}),e.jsx(r,{...t,mt:"xl",textType:"note",children:e.jsx("p",{children:"Note size & leading default value"})}),e.jsx(r,{...t,mt:"xl",textType:"note",textSize:"sm",textLeading:"default",children:e.jsx("p",{children:"Note Small Default"})}),e.jsx(r,{...t,mt:"md",textType:"note",textSize:"sm",textLeading:"narrow",children:e.jsx("p",{children:"Note Small Narrow"})}),e.jsx(r,{...t,mt:"md",textType:"note",textSize:"sm",textLeading:"tight",children:e.jsx("p",{children:"Note Small Tight"})}),e.jsx(r,{...t,mt:"xl",textType:"note",textSize:"md",textLeading:"default",children:e.jsx("p",{children:"Note Medium Default"})}),e.jsx(r,{...t,mt:"md",textType:"note",textSize:"md",textLeading:"narrow",children:e.jsx("p",{children:"Note Medium Narrow"})}),e.jsx(r,{...t,mt:"md",textType:"note",textSize:"md",textLeading:"tight",children:e.jsx("p",{children:"Note Medium Tight"})}),e.jsx(r,{...t,mt:"xl",textType:"note",textSize:"lg",textLeading:"default",children:e.jsx("p",{children:"Note Large Default"})}),e.jsx(r,{...t,mt:"md",textType:"note",textSize:"lg",textLeading:"narrow",children:e.jsx("p",{children:"Note Large Narrow"})}),e.jsx(r,{...t,mt:"md",textType:"note",textSize:"lg",textLeading:"tight",children:e.jsx("p",{children:"Note Large Tight"})}),e.jsx(r,{...n,mt:"xl",textAlign:"left",children:e.jsx("p",{children:"Text Left"})}),e.jsx(r,{...n,mt:"md",textAlign:"center",children:e.jsx("p",{children:"Text Center"})}),e.jsx(r,{...n,mt:"md",textAlign:"right",children:e.jsx("p",{children:"Text Right"})}),e.jsx("div",{style:{textAlign:"center"},children:e.jsx(r,{...n,mt:"md",children:e.jsx("p",{children:"Inherit(undefined)"})})})]}),args:n};var c,g,u;d.parameters={...d.parameters,docs:{...(c=d.parameters)==null?void 0:c.docs,source:{originalSource:`{
+  args: defaultArgs
+}`,...(u=(g=d.parameters)==null?void 0:g.docs)==null?void 0:u.source}}};var h,B,y;o.parameters={...o.parameters,docs:{...(h=o.parameters)==null?void 0:h.docs,source:{originalSource:`{
+  render: args => <div>
+      <Box {...args} backgroundColor="primary">
+        Primary
+      </Box>
+      <Box {...args} mt="md" backgroundColor="primaryDarken">
+        Primary Darken
+      </Box>
+      <Box {...args} mt="md" backgroundColor="accent">
+        Accent
+      </Box>
+      <Box {...args} mt="md" backgroundColor="accentDarken">
+        Accent Darken
+      </Box>
+      <Box {...args} mt="md" backgroundColor="alert">
+        Alert
+      </Box>
+      <Box {...args} mt="md" backgroundColor="gray">
+        Gray
+      </Box>
+      <Box {...args} mt="md" backgroundColor="white">
+        White
+      </Box>
+    </div>,
+  args: defaultArgs
+}`,...(y=(B=o.parameters)==null?void 0:B.docs)==null?void 0:y.source}}};var b,j,C;x.parameters={...x.parameters,docs:{...(b=x.parameters)==null?void 0:b.docs,source:{originalSource:`{
+  render: args => <div>
+      <Box {...args} pt="xxs" pr="xxs" pb="xxs" pl="xxs">
+        xxs
+      </Box>
+      <Box {...args} mt="md" pt="xs" pr="xs" pb="xs" pl="xs">
+        xs
+      </Box>
+      <Box {...args} mt="md" pt="sm" pr="sm" pb="sm" pl="sm">
+        sm
+      </Box>
+      <Box {...args} mt="md" pt="md" pr="md" pb="md" pl="md">
+        md
+      </Box>
+      <Box {...args} mt="md" pt="lg" pr="lg" pb="lg" pl="lg">
+        lg
+      </Box>
+      <Box {...args} mt="md" pt="xl" pr="xl" pb="xl" pl="xl">
+        xl
+      </Box>
+      <Box {...args} mt="md" pt="xxl" pr="xxl" pb="xxl" pl="xxl">
+        xxl
+      </Box>
+    </div>,
+  args: defaultArgs
+}`,...(C=(j=x.parameters)==null?void 0:j.docs)==null?void 0:C.source}}};var k,T,S;l.parameters={...l.parameters,docs:{...(k=l.parameters)==null?void 0:k.docs,source:{originalSource:`{
+  render: args => <div>
+      <Box {...args} mt="xxs" mr="xxs" mb="xxs" ml="xxs">
+        xxs
+      </Box>
+      <hr />
+      <Box {...args} mt="xs" mr="xs" mb="xs" ml="xs">
+        xs
+      </Box>
+      <hr />
+      <Box {...args} mt="sm" mr="sm" mb="sm" ml="sm">
+        sm
+      </Box>
+      <hr />
+      <Box {...args} mt="md" mr="md" mb="md" ml="md">
+        md
+      </Box>
+      <hr />
+      <Box {...args} mt="lg" mr="lg" mb="lg" ml="lg">
+        lg
+      </Box>
+      <hr />
+      <Box {...args} mt="xl" mr="xl" mb="xl" ml="xl">
+        xl
+      </Box>
+      <hr />
+      <Box {...args} mt="xxl" mr="xxl" mb="xxl" ml="xxl">
+        xxl
+      </Box>
+    </div>,
+  args: defaultArgs
+}`,...(S=(T=l.parameters)==null?void 0:T.docs)==null?void 0:S.source}}};var f,L,z;m.parameters={...m.parameters,docs:{...(f=m.parameters)==null?void 0:f.docs,source:{originalSource:`{
+  render: args => <div>
+      <Box {...args} pt="md" pr="md" pb="md" pl="md" radius="xs">
+        xs
+      </Box>
+      <Box {...args} mt="md" pt="md" pr="md" pb="md" pl="md" radius="sm">
+        sm
+      </Box>
+      <Box {...args} mt="md" pt="md" pr="md" pb="md" pl="md" radius="md">
+        md
+      </Box>
+      <Box {...args} mt="md" pt="md" pr="md" pb="md" pl="md" radius="lg">
+        lg
+      </Box>
+    </div>,
+  args: defaultArgs
+}`,...(z=(L=m.parameters)==null?void 0:L.docs)==null?void 0:z.source}}};var w,A,N;a.parameters={...a.parameters,docs:{...(w=a.parameters)==null?void 0:w.docs,source:{originalSource:`{
+  render: args => <div>
+      <Box {...args} backgroundColor="white" border="gray">
+        Border Gray
+      </Box>
+
+      <Box {...args} backgroundColor="white" border="gray">
+        Border Gray
+      </Box>
+      <Box mt="md" {...args} backgroundColor="white" border="grayThick">
+        Border Gray Thick
+      </Box>
+      <Box {...args} mt="md" backgroundColor="white" border="primary">
+        Primary Border
+      </Box>
+      <Box {...args} mt="md" backgroundColor="white" border="primaryThick">
+        Primary Border Thick
+      </Box>
+    </div>,
+  args: defaultArgs
+}`,...(N=(A=a.parameters)==null?void 0:A.docs)==null?void 0:N.source}}};var D,v,M;i.parameters={...i.parameters,docs:{...(D=i.parameters)==null?void 0:D.docs,source:{originalSource:`{
+  render: () => <Stack spacing="md">
+      <p>
+        Flexコンテナの子要素では、デフォルトでは横幅がintrinsic（コンテンツに応じた幅）となります。widthをfullとすることで100%を維持できます。
+      </p>
+      <Box {...defaultArgs} width="full">
+        Width Full
+      </Box>
+
+      <Box {...defaultArgs}>Not Width Full</Box>
+    </Stack>
+}`,...(M=(v=i.parameters)==null?void 0:v.docs)==null?void 0:M.source}}};var F,P,E;s.parameters={...s.parameters,docs:{...(F=s.parameters)==null?void 0:F.docs,source:{originalSource:`{
+  render: () => <Box as="section" pt="md" pr="md" pb="md" pl="md" radius="md" backgroundColor="primary">
+      <h2>Heading</h2>
+
+      <p>body</p>
+    </Box>
+}`,...(E=(P=s.parameters)==null?void 0:P.docs)==null?void 0:E.source}}};var G,W,R;p.parameters={...p.parameters,docs:{...(G=p.parameters)==null?void 0:G.docs,source:{originalSource:`{
+  render: args => <div>
+      <Box {...args} backgroundColor="gray" textColor="primary" textBold textType="body" textSize="lg" textLeading="narrow">
+        <p>Text Bold</p>
+
+        <Box {...args} backgroundColor="gray">
+          <p>nested</p>
+        </Box>
+
+        <Box {...args} mt="md" backgroundColor="gray" textType="note" textSize="lg" textLeading="tight" textBold={false}>
+          <p>nested. reset styles</p>
+        </Box>
+      </Box>
+
+      <Box {...args} backgroundColor="gray" mt="xl" textColor="main">
+        <p>Color Main</p>
+      </Box>
+      <Box {...args} backgroundColor="gray" mt="md" textColor="sub">
+        <p>Color Sub</p>
+      </Box>
+      <Box {...args} backgroundColor="gray" mt="md" textColor="link">
+        <p>Color Main</p>
+      </Box>
+      <Box {...args} backgroundColor="gray" mt="md" textColor="linkSub">
+        <p>Color Link Sub</p>
+      </Box>
+      <Box {...args} backgroundColor="gray" mt="md" textColor="disabled">
+        <p>Color Disabled</p>
+      </Box>
+      <Box {...args} backgroundColor="gray" mt="md" textColor="primary">
+        <p>Color Primary</p>
+      </Box>
+      <Box {...args} backgroundColor="gray" mt="md" textColor="accent">
+        <p>Color Accent</p>
+      </Box>
+      <Box {...args} backgroundColor="gray" mt="md" textColor="alert">
+        <p>Color Alert</p>
+      </Box>
+      <Box {...args} mt="xl" textType="body">
+        <p>Body size & leading default value</p>
+      </Box>
+
+      <Box {...args} mt="xl" textType="body" textSize="sm" textLeading="default">
+        <p>Body Small Default</p>
+      </Box>
+      <Box {...args} mt="md" textType="body" textSize="sm" textLeading="narrow">
+        <p>Body Small Narrow</p>
+      </Box>
+      <Box {...args} mt="md" textType="body" textSize="sm" textLeading="tight">
+        <p>Body Small Tight</p>
+      </Box>
+
+      <Box {...args} mt="xl" textType="body" textSize="md" textLeading="default">
+        <p>Body Medium Default</p>
+      </Box>
+      <Box {...args} mt="md" textType="body" textSize="md" textLeading="narrow">
+        <p>Body Medium Narrow</p>
+      </Box>
+      <Box {...args} mt="md" textType="body" textSize="md" textLeading="tight">
+        <p>Body Medium Tight</p>
+      </Box>
+
+      <Box {...args} mt="xl" textType="body" textSize="lg" textLeading="default">
+        <p>Body Large Default</p>
+      </Box>
+      <Box {...args} mt="md" textType="body" textSize="lg" textLeading="narrow">
+        <p>Body Large Narrow</p>
+      </Box>
+      <Box {...args} mt="md" textType="body" textSize="lg" textLeading="tight">
+        <p>Body Large Tight</p>
+      </Box>
+
+      <Box {...args} mt="xl" textType="note">
+        <p>Note size & leading default value</p>
+      </Box>
+
+      <Box {...args} mt="xl" textType="note" textSize="sm" textLeading="default">
+        <p>Note Small Default</p>
+      </Box>
+      <Box {...args} mt="md" textType="note" textSize="sm" textLeading="narrow">
+        <p>Note Small Narrow</p>
+      </Box>
+      <Box {...args} mt="md" textType="note" textSize="sm" textLeading="tight">
+        <p>Note Small Tight</p>
+      </Box>
+
+      <Box {...args} mt="xl" textType="note" textSize="md" textLeading="default">
+        <p>Note Medium Default</p>
+      </Box>
+      <Box {...args} mt="md" textType="note" textSize="md" textLeading="narrow">
+        <p>Note Medium Narrow</p>
+      </Box>
+      <Box {...args} mt="md" textType="note" textSize="md" textLeading="tight">
+        <p>Note Medium Tight</p>
+      </Box>
+
+      <Box {...args} mt="xl" textType="note" textSize="lg" textLeading="default">
+        <p>Note Large Default</p>
+      </Box>
+      <Box {...args} mt="md" textType="note" textSize="lg" textLeading="narrow">
+        <p>Note Large Narrow</p>
+      </Box>
+      <Box {...args} mt="md" textType="note" textSize="lg" textLeading="tight">
+        <p>Note Large Tight</p>
+      </Box>
+
+      <Box {...defaultArgs} mt="xl" textAlign="left">
+        <p>Text Left</p>
+      </Box>
+      <Box {...defaultArgs} mt="md" textAlign="center">
+        <p>Text Center</p>
+      </Box>
+      <Box {...defaultArgs} mt="md" textAlign="right">
+        <p>Text Right</p>
+      </Box>
+      <div style={{
+      textAlign: 'center'
+    }}>
+        <Box {...defaultArgs} mt="md">
+          <p>Inherit(undefined)</p>
+        </Box>
+      </div>
+    </div>,
+  args: defaultArgs
+}`,...(R=(W=p.parameters)==null?void 0:W.docs)==null?void 0:R.source}}};const Q=["Default","BackgroundColor","Padding","Margin","Radius","Border","Width","AsSection","TextVariations"];export{s as AsSection,o as BackgroundColor,a as Border,d as Default,l as Margin,x as Padding,m as Radius,p as TextVariations,i as Width,Q as __namedExportsOrder,K as default};
---
+++ assets/WithTooltip-Y7J54OF7-ac8adf5f.js
@@ -0,0 +1,1 @@
+import{W as h,W as l,b as s}from"./DocsRenderer-K4EAMTCU-7017e45d.js";import"./iframe-f2e23a07.js";import"../sb-preview/runtime.js";import"./index-1b03fe98.js";import"./react-18-5df836b6.js";import"./index-6fd5a17b.js";import"./index-d8983a70.js";import"./_getPrototype-dd248945.js";import"./index-356e4a49.js";export{h as WithToolTipState,l as WithTooltip,s as WithTooltipPure};
