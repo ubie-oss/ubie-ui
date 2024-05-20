991420f512e1eef7d0322b56e28f673a2a03ace7
hiroki.yokouchi <hiroki.yokouchi@dr-ubie.com>
2024-05-20 13:30:04 +0900

    chore: add @testing-library/user-event
    Signed-off-by: hiroki.yokouchi <hiroki.yokouchi@dr-ubie.com>

--- vitest.setup.ts
+++ vitest.setup.ts
@@ -3,3 +3,15 @@ import { setProjectAnnotations } from '@storybook/react';
 import * as globalStorybookConfig from './.storybook/preview';
 
 setProjectAnnotations(globalStorybookConfig);
+
+global.ResizeObserver = class ResizeObserver {
+  observe() {
+    // do nothing
+  }
+  unobserve() {
+    // do nothing
+  }
+  disconnect() {
+    // do nothing
+  }
+};
