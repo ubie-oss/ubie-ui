ac89706da6c474b7aca6f5fd053f6da3c47d0554
hiroki.yokouchi <hiroki.yokouchi@dr-ubie.com>
2024-05-23 17:05:12 +0900

    CustomDataAttribute and Id prop implementation was missing.

--- src/components/Color/Color.tsx
+++ src/components/Color/Color.tsx
@@ -2,6 +2,7 @@ import { type FC, PropsWithChildren } from 'react';
 import styles from './Color.module.css';
 import { TextColor } from '../../types/style';
 import { colorVariable } from '../../utils/style';
+import { CustomDataAttributeProps } from '../../types/attributes';
 
 type Props = {
   /**
@@ -9,10 +10,14 @@ type Props = {
    * @default main
    */
   color?: TextColor;
-};
+  /**
+   * ネイティブ要素の`id`属性。ページで固有のIDを指定
+   */
+  id?: string;
+} & CustomDataAttributeProps;
 
-export const Color: FC<PropsWithChildren<Props>> = ({ color = 'main', children }) => (
-  <span className={styles.color} style={{ ...colorVariable(color) }}>
+export const Color: FC<PropsWithChildren<Props>> = ({ color = 'main', children, id, ...otherProps }) => (
+  <span id={id} className={styles.color} style={{ ...colorVariable(color) }} {...otherProps}>
     {children}
   </span>
 );
