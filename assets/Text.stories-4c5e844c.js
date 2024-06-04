60d94ecbc555fc6a4ffabbb1bf075133257e0d4a
takanorip <takanorip@users.noreply.github.com>
2024-06-04 06:58:21 +0000

    deploy: a0bdc37c96cd41bb87bb096761aecea7bae9125e

---
+++ assets/Text.stories-4c5e844c.js
@@ -0,0 +1,275 @@
+import{j as e}from"./jsx-runtime-69eee039.js";import{T as n}from"./Text-b9f311a8.js";import{F as s}from"./Flex-bb8b913b.js";import{S as T}from"./Stack-79a82653.js";import"./index-7c191284.js";import"./clsx-1229b3e0.js";import"./style-e9a2251f.js";const se={title:"Typography/Text",component:n},r={},d={render:()=>e.jsx(n,{as:"h1",type:"heading",size:"xl",color:"primary",children:"Dummy Text"}),args:r},i={render:()=>e.jsxs(s,{spacing:"md",children:[e.jsx(n,{color:"main",children:"main"}),e.jsx(n,{color:"sub",children:"sub"}),e.jsx(n,{color:"link",children:"link"}),e.jsx(n,{color:"linkSub",children:"linkSub"}),e.jsx(n,{color:"disabled",children:"disabled"}),e.jsx(n,{color:"primary",children:"primary"}),e.jsx(n,{color:"accent",children:"accent"}),e.jsx(n,{color:"alert",children:"alert"}),e.jsx("span",{style:{background:"black",padding:"0 0.5em"},children:e.jsx(n,{color:"white",children:"white"})})]}),args:r},a={render:()=>{const t="近くの医療機関から調べる";return e.jsxs(s,{spacing:"md",alignItems:"center",children:[e.jsxs(n,{bold:!0,type:"heading",size:"xs",children:["xs",e.jsx("br",{}),`${t}`]}),e.jsxs(n,{bold:!0,type:"heading",size:"sm",children:["sm",e.jsx("br",{}),`${t}`]}),e.jsxs(n,{bold:!0,type:"heading",size:"md",children:["md",e.jsx("br",{}),`${t}`]}),e.jsxs(n,{bold:!0,type:"heading",size:"lg",children:["lg",e.jsx("br",{}),`${t}`]}),e.jsxs(n,{bold:!0,type:"heading",size:"xl",children:["xl",e.jsx("br",{}),`${t}`]})]})},args:r},l={render:()=>{const t="症状検索エンジン「ユビー」は「適切なタイミングでの適切な医療との出会い」の橋渡しによって、みなさんの健康・安全をサポートし、医療についての不安・悩み・苦痛を和らげます。";return e.jsxs(T,{as:"dl",spacing:"md",children:[e.jsxs("div",{children:[e.jsx("dt",{children:"Default Leading"}),e.jsx("dd",{children:e.jsxs(s,{spacing:"md",alignItems:"center",children:[e.jsxs(n,{type:"body",size:"sm",children:["sm",e.jsx("br",{}),`${t}`]}),e.jsxs(n,{type:"body",size:"md",children:["md",e.jsx("br",{}),`${t}`]}),e.jsxs(n,{type:"body",size:"lg",children:["lg",e.jsx("br",{}),`${t}`]})]})})]}),e.jsxs("div",{children:[e.jsx("dt",{children:"Narrow Leading"}),e.jsx("dd",{children:e.jsxs(s,{spacing:"md",alignItems:"center",children:[e.jsxs(n,{leading:"narrow",type:"body",size:"sm",children:["sm",e.jsx("br",{}),`${t}`]}),e.jsxs(n,{leading:"narrow",type:"body",size:"md",children:["md",e.jsx("br",{}),`${t}`]}),e.jsxs(n,{leading:"narrow",type:"body",size:"lg",children:["lg",e.jsx("br",{}),`${t}`]})]})})]}),e.jsxs("div",{children:[e.jsx("dt",{children:"Tight Leading"}),e.jsx("dd",{children:e.jsxs(s,{spacing:"md",alignItems:"center",children:[e.jsxs(n,{leading:"tight",type:"body",size:"sm",children:["sm",e.jsx("br",{}),`${t}`]}),e.jsxs(n,{leading:"tight",type:"body",size:"md",children:["md",e.jsx("br",{}),`${t}`]}),e.jsxs(n,{leading:"tight",type:"body",size:"lg",children:["lg",e.jsx("br",{}),`${t}`]})]})})]})]})},args:r},x={render:()=>{const t="症状検索エンジン「ユビー」は「適切なタイミングでの適切な医療との出会い」の橋渡しによって、みなさんの健康・安全をサポートし、医療についての不安・悩み・苦痛を和らげます。";return e.jsxs(T,{as:"dl",spacing:"md",children:[e.jsxs("div",{children:[e.jsx("dt",{children:"Default Leading"}),e.jsx("dd",{children:e.jsxs(s,{spacing:"md",alignItems:"center",children:[e.jsxs(n,{type:"note",size:"sm",children:["sm",e.jsx("br",{}),`${t}`]}),e.jsxs(n,{type:"note",size:"md",children:["md",e.jsx("br",{}),`${t}`]}),e.jsxs(n,{type:"note",size:"lg",children:["lg",e.jsx("br",{}),`${t}`]})]})})]}),e.jsxs("div",{children:[e.jsx("dt",{children:"Narrow Leading"}),e.jsx("dd",{children:e.jsxs(s,{spacing:"md",alignItems:"center",children:[e.jsxs(n,{leading:"narrow",type:"note",size:"sm",children:["sm",e.jsx("br",{}),`${t}`]}),e.jsxs(n,{leading:"narrow",type:"note",size:"md",children:["md",e.jsx("br",{}),`${t}`]}),e.jsxs(n,{leading:"narrow",type:"note",size:"lg",children:["lg",e.jsx("br",{}),`${t}`]})]})})]}),e.jsxs("div",{children:[e.jsx("dt",{children:"Tight Leading"}),e.jsx("dd",{children:e.jsxs(s,{spacing:"md",alignItems:"center",children:[e.jsxs(n,{leading:"tight",type:"note",size:"sm",children:["sm",e.jsx("br",{}),`${t}`]}),e.jsxs(n,{leading:"tight",type:"note",size:"md",children:["md",e.jsx("br",{}),`${t}`]}),e.jsxs(n,{leading:"tight",type:"note",size:"lg",children:["lg",e.jsx("br",{}),`${t}`]})]})})]})]})},args:r},u={render:()=>{const t="同意して症状から調べる";return e.jsxs(s,{spacing:"md",alignItems:"center",children:[e.jsxs(n,{type:"button",size:"sm",children:["sm",e.jsx("br",{}),`${t}`]}),e.jsxs(n,{type:"button",size:"md",children:["md",e.jsx("br",{}),`${t}`]}),e.jsxs(n,{type:"button",size:"lg",children:["lg",e.jsx("br",{}),`${t}`]})]})},args:r},o={render:()=>{const t="循環器内科";return e.jsxs(s,{spacing:"md",alignItems:"center",children:[e.jsxs(n,{type:"tag",size:"sm",children:["sm",e.jsx("br",{}),`${t}`]}),e.jsxs(n,{type:"tag",size:"md",children:["md",e.jsx("br",{}),`${t}`]}),e.jsxs(n,{type:"tag",size:"lg",children:["lg",e.jsx("br",{}),`${t}`]})]})},args:r},c={render:()=>e.jsx(n,{id:"text-id",type:"heading",size:"xl",as:"h2",color:"primary",bold:!0,children:"Dummy Text"})},g={render:()=>e.jsxs(n,{type:"note",size:"lg",children:[e.jsx(n,{as:"span",type:"note",size:"lg",color:"alert",bold:!0,children:"Alert:"})," ","Please fill in all fields"]})},m={render:()=>e.jsxs(T,{spacing:"md",alignItems:"normal",children:[e.jsx(n,{textAlign:"left",children:"Left"}),e.jsx(n,{textAlign:"center",children:"Center"}),e.jsx(n,{textAlign:"right",children:"Right"}),e.jsx("div",{style:{textAlign:"center"},children:e.jsx(n,{textAlign:"center",children:"Inherit(undefined)"})})]})},p={render:()=>e.jsx(n,{"date-test-id":"some-id",bold:!0,children:"Text"})};var h,b,y;d.parameters={...d.parameters,docs:{...(h=d.parameters)==null?void 0:h.docs,source:{originalSource:`{
+  render: () => <Text as="h1" type="heading" size="xl" color="primary">
+      Dummy Text
+    </Text>,
+  args: defaultArgs
+}`,...(y=(b=d.parameters)==null?void 0:b.docs)==null?void 0:y.source}}};var j,z,F;i.parameters={...i.parameters,docs:{...(j=i.parameters)==null?void 0:j.docs,source:{originalSource:`{
+  render: () => <Flex spacing="md">
+      <Text color="main">main</Text>
+      <Text color="sub">sub</Text>
+      <Text color="link">link</Text>
+      <Text color="linkSub">linkSub</Text>
+      <Text color="disabled">disabled</Text>
+      <Text color="primary">primary</Text>
+      <Text color="accent">accent</Text>
+      <Text color="alert">alert</Text>
+      <span style={{
+      background: 'black',
+      padding: '0 0.5em'
+    }}>
+        <Text color="white">white</Text>
+      </span>
+    </Flex>,
+  args: defaultArgs
+}`,...(F=(z=i.parameters)==null?void 0:z.docs)==null?void 0:F.source}}};var $,A,B;a.parameters={...a.parameters,docs:{...($=a.parameters)==null?void 0:$.docs,source:{originalSource:`{
+  render: () => {
+    const headingText = '近くの医療機関から調べる';
+    return <Flex spacing="md" alignItems="center">
+        <Text bold type="heading" size="xs">
+          xs
+          <br />
+          {\`\${headingText}\`}
+        </Text>
+        <Text bold type="heading" size="sm">
+          sm
+          <br />
+          {\`\${headingText}\`}
+        </Text>
+        <Text bold type="heading" size="md">
+          md
+          <br />
+          {\`\${headingText}\`}
+        </Text>
+        <Text bold type="heading" size="lg">
+          lg
+          <br />
+          {\`\${headingText}\`}
+        </Text>
+        <Text bold type="heading" size="xl">
+          xl
+          <br />
+          {\`\${headingText}\`}
+        </Text>
+      </Flex>;
+  },
+  args: defaultArgs
+}`,...(B=(A=a.parameters)==null?void 0:A.docs)==null?void 0:B.source}}};var D,f,I;l.parameters={...l.parameters,docs:{...(D=l.parameters)==null?void 0:D.docs,source:{originalSource:`{
+  render: () => {
+    const bodyText = '症状検索エンジン「ユビー」は「適切なタイミングでの適切な医療との出会い」の橋渡しによって、みなさんの健康・安全をサポートし、医療についての不安・悩み・苦痛を和らげます。';
+    return <Stack as="dl" spacing="md">
+        <div>
+          <dt>Default Leading</dt>
+          <dd>
+            <Flex spacing="md" alignItems="center">
+              <Text type="body" size="sm">
+                sm
+                <br />
+                {\`\${bodyText}\`}
+              </Text>
+              <Text type="body" size="md">
+                md
+                <br />
+                {\`\${bodyText}\`}
+              </Text>
+              <Text type="body" size="lg">
+                lg
+                <br />
+                {\`\${bodyText}\`}
+              </Text>
+            </Flex>
+          </dd>
+        </div>
+
+        <div>
+          <dt>Narrow Leading</dt>
+          <dd>
+            <Flex spacing="md" alignItems="center">
+              <Text leading="narrow" type="body" size="sm">
+                sm
+                <br />
+                {\`\${bodyText}\`}
+              </Text>
+              <Text leading="narrow" type="body" size="md">
+                md
+                <br />
+                {\`\${bodyText}\`}
+              </Text>
+              <Text leading="narrow" type="body" size="lg">
+                lg
+                <br />
+                {\`\${bodyText}\`}
+              </Text>
+            </Flex>
+          </dd>
+        </div>
+
+        <div>
+          <dt>Tight Leading</dt>
+          <dd>
+            <Flex spacing="md" alignItems="center">
+              <Text leading="tight" type="body" size="sm">
+                sm
+                <br />
+                {\`\${bodyText}\`}
+              </Text>
+              <Text leading="tight" type="body" size="md">
+                md
+                <br />
+                {\`\${bodyText}\`}
+              </Text>
+              <Text leading="tight" type="body" size="lg">
+                lg
+                <br />
+                {\`\${bodyText}\`}
+              </Text>
+            </Flex>
+          </dd>
+        </div>
+      </Stack>;
+  },
+  args: defaultArgs
+}`,...(I=(f=l.parameters)==null?void 0:f.docs)==null?void 0:I.source}}};var E,C,S;x.parameters={...x.parameters,docs:{...(E=x.parameters)==null?void 0:E.docs,source:{originalSource:`{
+  render: () => {
+    const noteText = '症状検索エンジン「ユビー」は「適切なタイミングでの適切な医療との出会い」の橋渡しによって、みなさんの健康・安全をサポートし、医療についての不安・悩み・苦痛を和らげます。';
+    return <Stack as="dl" spacing="md">
+        <div>
+          <dt>Default Leading</dt>
+          <dd>
+            <Flex spacing="md" alignItems="center">
+              <Text type="note" size="sm">
+                sm
+                <br />
+                {\`\${noteText}\`}
+              </Text>
+              <Text type="note" size="md">
+                md
+                <br />
+                {\`\${noteText}\`}
+              </Text>
+              <Text type="note" size="lg">
+                lg
+                <br />
+                {\`\${noteText}\`}
+              </Text>
+            </Flex>
+          </dd>
+        </div>
+
+        <div>
+          <dt>Narrow Leading</dt>
+          <dd>
+            <Flex spacing="md" alignItems="center">
+              <Text leading="narrow" type="note" size="sm">
+                sm
+                <br />
+                {\`\${noteText}\`}
+              </Text>
+              <Text leading="narrow" type="note" size="md">
+                md
+                <br />
+                {\`\${noteText}\`}
+              </Text>
+              <Text leading="narrow" type="note" size="lg">
+                lg
+                <br />
+                {\`\${noteText}\`}
+              </Text>
+            </Flex>
+          </dd>
+        </div>
+        <div>
+          <dt>Tight Leading</dt>
+          <dd>
+            <Flex spacing="md" alignItems="center">
+              <Text leading="tight" type="note" size="sm">
+                sm
+                <br />
+                {\`\${noteText}\`}
+              </Text>
+              <Text leading="tight" type="note" size="md">
+                md
+                <br />
+                {\`\${noteText}\`}
+              </Text>
+              <Text leading="tight" type="note" size="lg">
+                lg
+                <br />
+                {\`\${noteText}\`}
+              </Text>
+            </Flex>
+          </dd>
+        </div>
+      </Stack>;
+  },
+  args: defaultArgs
+}`,...(S=(C=x.parameters)==null?void 0:C.docs)==null?void 0:S.source}}};var v,w,k;u.parameters={...u.parameters,docs:{...(v=u.parameters)==null?void 0:v.docs,source:{originalSource:`{
+  render: () => {
+    const buttonText = '同意して症状から調べる';
+    return <Flex spacing="md" alignItems="center">
+        <Text type="button" size="sm">
+          sm
+          <br />
+          {\`\${buttonText}\`}
+        </Text>
+        <Text type="button" size="md">
+          md
+          <br />
+          {\`\${buttonText}\`}
+        </Text>
+        <Text type="button" size="lg">
+          lg
+          <br />
+          {\`\${buttonText}\`}
+        </Text>
+      </Flex>;
+  },
+  args: defaultArgs
+}`,...(k=(w=u.parameters)==null?void 0:w.docs)==null?void 0:k.source}}};var L,N,R;o.parameters={...o.parameters,docs:{...(L=o.parameters)==null?void 0:L.docs,source:{originalSource:`{
+  render: () => {
+    const tagText = '循環器内科';
+    return <Flex spacing="md" alignItems="center">
+        <Text type="tag" size="sm">
+          sm
+          <br />
+          {\`\${tagText}\`}
+        </Text>
+        <Text type="tag" size="md">
+          md
+          <br />
+          {\`\${tagText}\`}
+        </Text>
+        <Text type="tag" size="lg">
+          lg
+          <br />
+          {\`\${tagText}\`}
+        </Text>
+      </Flex>;
+  },
+  args: defaultArgs
+}`,...(R=(N=o.parameters)==null?void 0:N.docs)==null?void 0:R.source}}};var _,H,P;c.parameters={...c.parameters,docs:{...(_=c.parameters)==null?void 0:_.docs,source:{originalSource:`{
+  render: () => <Text id="text-id" type="heading" size="xl" as="h2" color="primary" bold>
+      Dummy Text
+    </Text>
+}`,...(P=(H=c.parameters)==null?void 0:H.docs)==null?void 0:P.source}}};var W,O,q;g.parameters={...g.parameters,docs:{...(W=g.parameters)==null?void 0:W.docs,source:{originalSource:`{
+  render: () => <Text type="note" size="lg">
+      <Text as="span" type="note" size="lg" color="alert" bold>
+        Alert:
+      </Text>{' '}
+      Please fill in all fields
+    </Text>
+}`,...(q=(O=g.parameters)==null?void 0:O.docs)==null?void 0:q.source}}};var G,J,K;m.parameters={...m.parameters,docs:{...(G=m.parameters)==null?void 0:G.docs,source:{originalSource:`{
+  render: () => <Stack spacing="md" alignItems="normal">
+      <Text textAlign="left">Left</Text>
+      <Text textAlign="center">Center</Text>
+      <Text textAlign="right">Right</Text>
+      <div style={{
+      textAlign: 'center'
+    }}>
+        <Text textAlign="center">Inherit(undefined)</Text>
+      </div>
+    </Stack>
+}`,...(K=(J=m.parameters)==null?void 0:J.docs)==null?void 0:K.source}}};var M,Q,U;p.parameters={...p.parameters,docs:{...(M=p.parameters)==null?void 0:M.docs,source:{originalSource:`{
+  render: () => <Text date-test-id="some-id" bold>
+      Text
+    </Text>
+}`,...(U=(Q=p.parameters)==null?void 0:Q.docs)==null?void 0:U.source}}};const re=["Default","Color","Heading","Body","Note","Button","Tag","WithId","TextInText","TextAlign","CustomDataAttribute"];export{l as Body,u as Button,i as Color,p as CustomDataAttribute,d as Default,a as Heading,x as Note,o as Tag,m as TextAlign,g as TextInText,c as WithId,re as __namedExportsOrder,se as default};
