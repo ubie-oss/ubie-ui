c5b3f807f98c805db9d52b27ff7a4645f705d361
takanorip <takanorip@users.noreply.github.com>
2024-06-07 06:34:29 +0000

    deploy: 01577393c403c7ef9ba6b7570142e8c3cc53a0ca

---
+++ assets/Box.stories-85d2801a.js
@@ -0,0 +1,267 @@
+import{j as e}from"./jsx-runtime-69eee039.js";import{B as r}from"./Box-7eff7901.js";import{S as O}from"./Stack-79a82653.js";import"./index-7c191284.js";import"./clsx-1229b3e0.js";import"./style-e9a2251f.js";const Y={component:r},n={children:"Box",pt:"md",pr:"md",pb:"md",pl:"md",radius:"md",backgroundColor:"primary"},d={args:n},o={render:t=>e.jsxs("div",{children:[e.jsx(r,{...t,backgroundColor:"primary",children:"Primary"}),e.jsx(r,{...t,mt:"md",backgroundColor:"primaryDarken",children:"Primary Darken"}),e.jsx(r,{...t,mt:"md",backgroundColor:"accent",children:"Accent"}),e.jsx(r,{...t,mt:"md",backgroundColor:"accentDarken",children:"Accent Darken"}),e.jsx(r,{...t,mt:"md",backgroundColor:"alert",children:"Alert"}),e.jsx(r,{...t,mt:"md",backgroundColor:"gray",children:"Gray"}),e.jsx(r,{...t,mt:"md",backgroundColor:"white",children:"White"})]}),args:n},x={render:t=>e.jsxs("div",{children:[e.jsx(r,{...t,pt:"xxs",pr:"xxs",pb:"xxs",pl:"xxs",children:"xxs"}),e.jsx(r,{...t,mt:"md",pt:"xs",pr:"xs",pb:"xs",pl:"xs",children:"xs"}),e.jsx(r,{...t,mt:"md",pt:"sm",pr:"sm",pb:"sm",pl:"sm",children:"sm"}),e.jsx(r,{...t,mt:"md",pt:"md",pr:"md",pb:"md",pl:"md",children:"md"}),e.jsx(r,{...t,mt:"md",pt:"lg",pr:"lg",pb:"lg",pl:"lg",children:"lg"}),e.jsx(r,{...t,mt:"md",pt:"xl",pr:"xl",pb:"xl",pl:"xl",children:"xl"}),e.jsx(r,{...t,mt:"md",pt:"xxl",pr:"xxl",pb:"xxl",pl:"xxl",children:"xxl"})]}),args:n},l={render:t=>e.jsxs("div",{children:[e.jsx(r,{...t,mt:"xxs",mr:"xxs",mb:"xxs",ml:"xxs",children:"xxs"}),e.jsx("hr",{}),e.jsx(r,{...t,mt:"xs",mr:"xs",mb:"xs",ml:"xs",children:"xs"}),e.jsx("hr",{}),e.jsx(r,{...t,mt:"sm",mr:"sm",mb:"sm",ml:"sm",children:"sm"}),e.jsx("hr",{}),e.jsx(r,{...t,mt:"md",mr:"md",mb:"md",ml:"md",children:"md"}),e.jsx("hr",{}),e.jsx(r,{...t,mt:"lg",mr:"lg",mb:"lg",ml:"lg",children:"lg"}),e.jsx("hr",{}),e.jsx(r,{...t,mt:"xl",mr:"xl",mb:"xl",ml:"xl",children:"xl"}),e.jsx("hr",{}),e.jsx(r,{...t,mt:"xxl",mr:"xxl",mb:"xxl",ml:"xxl",children:"xxl"})]}),args:n},m={render:t=>e.jsxs("div",{children:[e.jsx(r,{...t,pt:"md",pr:"md",pb:"md",pl:"md",radius:"xs",children:"xs"}),e.jsx(r,{...t,mt:"md",pt:"md",pr:"md",pb:"md",pl:"md",radius:"sm",children:"sm"}),e.jsx(r,{...t,mt:"md",pt:"md",pr:"md",pb:"md",pl:"md",radius:"md",children:"md"}),e.jsx(r,{...t,mt:"md",pt:"md",pr:"md",pb:"md",pl:"md",radius:"lg",children:"lg"})]}),args:n},a={render:t=>e.jsxs("div",{children:[e.jsx(r,{...t,backgroundColor:"white",border:"gray",children:"Border Gray"}),e.jsx(r,{...t,backgroundColor:"white",border:"gray",children:"Border Gray"}),e.jsx(r,{mt:"md",...t,backgroundColor:"white",border:"grayThick",children:"Border Gray Thick"}),e.jsx(r,{...t,mt:"md",backgroundColor:"white",border:"primary",children:"Primary Border"}),e.jsx(r,{...t,mt:"md",backgroundColor:"white",border:"primaryThick",children:"Primary Border Thick"})]}),args:n},s={render:()=>e.jsxs(O,{spacing:"md",children:[e.jsx("p",{children:"Flexコンテナの子要素では、デフォルトでは横幅がintrinsic（コンテンツに応じた幅）となります。widthをfullとすることで100%を維持できます。"}),e.jsx(r,{...n,width:"full",children:"Width Full"}),e.jsx(r,{...n,children:"Not Width Full"})]})},i={render:()=>e.jsxs(r,{as:"section",pt:"md",pr:"md",pb:"md",pl:"md",radius:"md",backgroundColor:"primary",children:[e.jsx("h2",{children:"Heading"}),e.jsx("p",{children:"body"})]})},p={render:t=>e.jsxs("div",{children:[e.jsxs(r,{...t,backgroundColor:"gray",textColor:"primary",textBold:!0,textType:"body",textSize:"lg",textLeading:"narrow",children:[e.jsx("p",{children:"Text Bold"}),e.jsx(r,{...t,backgroundColor:"gray",children:e.jsx("p",{children:"nested"})}),e.jsx(r,{...t,mt:"md",backgroundColor:"gray",textType:"note",textSize:"lg",textLeading:"tight",textBold:!1,children:e.jsx("p",{children:"nested. reset styles"})})]}),e.jsx(r,{...t,backgroundColor:"gray",mt:"xl",textColor:"main",children:e.jsx("p",{children:"Color Main"})}),e.jsx(r,{...t,backgroundColor:"gray",mt:"md",textColor:"sub",children:e.jsx("p",{children:"Color Sub"})}),e.jsx(r,{...t,backgroundColor:"gray",mt:"md",textColor:"link",children:e.jsx("p",{children:"Color Main"})}),e.jsx(r,{...t,backgroundColor:"gray",mt:"md",textColor:"linkSub",children:e.jsx("p",{children:"Color Link Sub"})}),e.jsx(r,{...t,backgroundColor:"gray",mt:"md",textColor:"disabled",children:e.jsx("p",{children:"Color Disabled"})}),e.jsx(r,{...t,backgroundColor:"gray",mt:"md",textColor:"primary",children:e.jsx("p",{children:"Color Primary"})}),e.jsx(r,{...t,backgroundColor:"gray",mt:"md",textColor:"accent",children:e.jsx("p",{children:"Color Accent"})}),e.jsx(r,{...t,backgroundColor:"gray",mt:"md",textColor:"alert",children:e.jsx("p",{children:"Color Alert"})}),e.jsx(r,{...t,mt:"xl",textType:"body",children:e.jsx("p",{children:"Body size & leading default value"})}),e.jsx(r,{...t,mt:"xl",textType:"body",textSize:"sm",textLeading:"default",children:e.jsx("p",{children:"Body Small Default"})}),e.jsx(r,{...t,mt:"md",textType:"body",textSize:"sm",textLeading:"narrow",children:e.jsx("p",{children:"Body Small Narrow"})}),e.jsx(r,{...t,mt:"md",textType:"body",textSize:"sm",textLeading:"tight",children:e.jsx("p",{children:"Body Small Tight"})}),e.jsx(r,{...t,mt:"xl",textType:"body",textSize:"md",textLeading:"default",children:e.jsx("p",{children:"Body Medium Default"})}),e.jsx(r,{...t,mt:"md",textType:"body",textSize:"md",textLeading:"narrow",children:e.jsx("p",{children:"Body Medium Narrow"})}),e.jsx(r,{...t,mt:"md",textType:"body",textSize:"md",textLeading:"tight",children:e.jsx("p",{children:"Body Medium Tight"})}),e.jsx(r,{...t,mt:"xl",textType:"body",textSize:"lg",textLeading:"default",children:e.jsx("p",{children:"Body Large Default"})}),e.jsx(r,{...t,mt:"md",textType:"body",textSize:"lg",textLeading:"narrow",children:e.jsx("p",{children:"Body Large Narrow"})}),e.jsx(r,{...t,mt:"md",textType:"body",textSize:"lg",textLeading:"tight",children:e.jsx("p",{children:"Body Large Tight"})}),e.jsx(r,{...t,mt:"xl",textType:"note",children:e.jsx("p",{children:"Note size & leading default value"})}),e.jsx(r,{...t,mt:"xl",textType:"note",textSize:"sm",textLeading:"default",children:e.jsx("p",{children:"Note Small Default"})}),e.jsx(r,{...t,mt:"md",textType:"note",textSize:"sm",textLeading:"narrow",children:e.jsx("p",{children:"Note Small Narrow"})}),e.jsx(r,{...t,mt:"md",textType:"note",textSize:"sm",textLeading:"tight",children:e.jsx("p",{children:"Note Small Tight"})}),e.jsx(r,{...t,mt:"xl",textType:"note",textSize:"md",textLeading:"default",children:e.jsx("p",{children:"Note Medium Default"})}),e.jsx(r,{...t,mt:"md",textType:"note",textSize:"md",textLeading:"narrow",children:e.jsx("p",{children:"Note Medium Narrow"})}),e.jsx(r,{...t,mt:"md",textType:"note",textSize:"md",textLeading:"tight",children:e.jsx("p",{children:"Note Medium Tight"})}),e.jsx(r,{...t,mt:"xl",textType:"note",textSize:"lg",textLeading:"default",children:e.jsx("p",{children:"Note Large Default"})}),e.jsx(r,{...t,mt:"md",textType:"note",textSize:"lg",textLeading:"narrow",children:e.jsx("p",{children:"Note Large Narrow"})}),e.jsx(r,{...t,mt:"md",textType:"note",textSize:"lg",textLeading:"tight",children:e.jsx("p",{children:"Note Large Tight"})}),e.jsx(r,{...n,mt:"xl",textAlign:"left",children:e.jsx("p",{children:"Text Left"})}),e.jsx(r,{...n,mt:"md",textAlign:"center",children:e.jsx("p",{children:"Text Center"})}),e.jsx(r,{...n,mt:"md",textAlign:"right",children:e.jsx("p",{children:"Text Right"})}),e.jsx("div",{style:{textAlign:"center"},children:e.jsx(r,{...n,mt:"md",children:e.jsx("p",{children:"Inherit(undefined)"})})})]}),args:n},c={render:t=>e.jsx(r,{...t}),args:{"data-test-id":"some-test",...n}};var g,u,h;d.parameters={...d.parameters,docs:{...(g=d.parameters)==null?void 0:g.docs,source:{originalSource:`{
+  args: defaultArgs
+}`,...(h=(u=d.parameters)==null?void 0:u.docs)==null?void 0:h.source}}};var B,y,b;o.parameters={...o.parameters,docs:{...(B=o.parameters)==null?void 0:B.docs,source:{originalSource:`{
+  render: args => <div>
+      <Box {...args} backgroundColor="primary">
+        Primary
+      </Box>
+      <Box {...args} mt="md" backgroundColor="primaryDarken">
+        Primary Darken
+      </Box>
+      <Box {...args} mt="md" backgroundColor="accent">
+        Accent
+      </Box>
+      <Box {...args} mt="md" backgroundColor="accentDarken">
+        Accent Darken
+      </Box>
+      <Box {...args} mt="md" backgroundColor="alert">
+        Alert
+      </Box>
+      <Box {...args} mt="md" backgroundColor="gray">
+        Gray
+      </Box>
+      <Box {...args} mt="md" backgroundColor="white">
+        White
+      </Box>
+    </div>,
+  args: defaultArgs
+}`,...(b=(y=o.parameters)==null?void 0:y.docs)==null?void 0:b.source}}};var j,C,k;x.parameters={...x.parameters,docs:{...(j=x.parameters)==null?void 0:j.docs,source:{originalSource:`{
+  render: args => <div>
+      <Box {...args} pt="xxs" pr="xxs" pb="xxs" pl="xxs">
+        xxs
+      </Box>
+      <Box {...args} mt="md" pt="xs" pr="xs" pb="xs" pl="xs">
+        xs
+      </Box>
+      <Box {...args} mt="md" pt="sm" pr="sm" pb="sm" pl="sm">
+        sm
+      </Box>
+      <Box {...args} mt="md" pt="md" pr="md" pb="md" pl="md">
+        md
+      </Box>
+      <Box {...args} mt="md" pt="lg" pr="lg" pb="lg" pl="lg">
+        lg
+      </Box>
+      <Box {...args} mt="md" pt="xl" pr="xl" pb="xl" pl="xl">
+        xl
+      </Box>
+      <Box {...args} mt="md" pt="xxl" pr="xxl" pb="xxl" pl="xxl">
+        xxl
+      </Box>
+    </div>,
+  args: defaultArgs
+}`,...(k=(C=x.parameters)==null?void 0:C.docs)==null?void 0:k.source}}};var S,T,f;l.parameters={...l.parameters,docs:{...(S=l.parameters)==null?void 0:S.docs,source:{originalSource:`{
+  render: args => <div>
+      <Box {...args} mt="xxs" mr="xxs" mb="xxs" ml="xxs">
+        xxs
+      </Box>
+      <hr />
+      <Box {...args} mt="xs" mr="xs" mb="xs" ml="xs">
+        xs
+      </Box>
+      <hr />
+      <Box {...args} mt="sm" mr="sm" mb="sm" ml="sm">
+        sm
+      </Box>
+      <hr />
+      <Box {...args} mt="md" mr="md" mb="md" ml="md">
+        md
+      </Box>
+      <hr />
+      <Box {...args} mt="lg" mr="lg" mb="lg" ml="lg">
+        lg
+      </Box>
+      <hr />
+      <Box {...args} mt="xl" mr="xl" mb="xl" ml="xl">
+        xl
+      </Box>
+      <hr />
+      <Box {...args} mt="xxl" mr="xxl" mb="xxl" ml="xxl">
+        xxl
+      </Box>
+    </div>,
+  args: defaultArgs
+}`,...(f=(T=l.parameters)==null?void 0:T.docs)==null?void 0:f.source}}};var L,z,A;m.parameters={...m.parameters,docs:{...(L=m.parameters)==null?void 0:L.docs,source:{originalSource:`{
+  render: args => <div>
+      <Box {...args} pt="md" pr="md" pb="md" pl="md" radius="xs">
+        xs
+      </Box>
+      <Box {...args} mt="md" pt="md" pr="md" pb="md" pl="md" radius="sm">
+        sm
+      </Box>
+      <Box {...args} mt="md" pt="md" pr="md" pb="md" pl="md" radius="md">
+        md
+      </Box>
+      <Box {...args} mt="md" pt="md" pr="md" pb="md" pl="md" radius="lg">
+        lg
+      </Box>
+    </div>,
+  args: defaultArgs
+}`,...(A=(z=m.parameters)==null?void 0:z.docs)==null?void 0:A.source}}};var w,N,D;a.parameters={...a.parameters,docs:{...(w=a.parameters)==null?void 0:w.docs,source:{originalSource:`{
+  render: args => <div>
+      <Box {...args} backgroundColor="white" border="gray">
+        Border Gray
+      </Box>
+
+      <Box {...args} backgroundColor="white" border="gray">
+        Border Gray
+      </Box>
+      <Box mt="md" {...args} backgroundColor="white" border="grayThick">
+        Border Gray Thick
+      </Box>
+      <Box {...args} mt="md" backgroundColor="white" border="primary">
+        Primary Border
+      </Box>
+      <Box {...args} mt="md" backgroundColor="white" border="primaryThick">
+        Primary Border Thick
+      </Box>
+    </div>,
+  args: defaultArgs
+}`,...(D=(N=a.parameters)==null?void 0:N.docs)==null?void 0:D.source}}};var v,M,F;s.parameters={...s.parameters,docs:{...(v=s.parameters)==null?void 0:v.docs,source:{originalSource:`{
+  render: () => <Stack spacing="md">
+      <p>
+        Flexコンテナの子要素では、デフォルトでは横幅がintrinsic（コンテンツに応じた幅）となります。widthをfullとすることで100%を維持できます。
+      </p>
+      <Box {...defaultArgs} width="full">
+        Width Full
+      </Box>
+
+      <Box {...defaultArgs}>Not Width Full</Box>
+    </Stack>
+}`,...(F=(M=s.parameters)==null?void 0:M.docs)==null?void 0:F.source}}};var P,E,G;i.parameters={...i.parameters,docs:{...(P=i.parameters)==null?void 0:P.docs,source:{originalSource:`{
+  render: () => <Box as="section" pt="md" pr="md" pb="md" pl="md" radius="md" backgroundColor="primary">
+      <h2>Heading</h2>
+
+      <p>body</p>
+    </Box>
+}`,...(G=(E=i.parameters)==null?void 0:E.docs)==null?void 0:G.source}}};var W,R,_;p.parameters={...p.parameters,docs:{...(W=p.parameters)==null?void 0:W.docs,source:{originalSource:`{
+  render: args => <div>
+      <Box {...args} backgroundColor="gray" textColor="primary" textBold textType="body" textSize="lg" textLeading="narrow">
+        <p>Text Bold</p>
+
+        <Box {...args} backgroundColor="gray">
+          <p>nested</p>
+        </Box>
+
+        <Box {...args} mt="md" backgroundColor="gray" textType="note" textSize="lg" textLeading="tight" textBold={false}>
+          <p>nested. reset styles</p>
+        </Box>
+      </Box>
+
+      <Box {...args} backgroundColor="gray" mt="xl" textColor="main">
+        <p>Color Main</p>
+      </Box>
+      <Box {...args} backgroundColor="gray" mt="md" textColor="sub">
+        <p>Color Sub</p>
+      </Box>
+      <Box {...args} backgroundColor="gray" mt="md" textColor="link">
+        <p>Color Main</p>
+      </Box>
+      <Box {...args} backgroundColor="gray" mt="md" textColor="linkSub">
+        <p>Color Link Sub</p>
+      </Box>
+      <Box {...args} backgroundColor="gray" mt="md" textColor="disabled">
+        <p>Color Disabled</p>
+      </Box>
+      <Box {...args} backgroundColor="gray" mt="md" textColor="primary">
+        <p>Color Primary</p>
+      </Box>
+      <Box {...args} backgroundColor="gray" mt="md" textColor="accent">
+        <p>Color Accent</p>
+      </Box>
+      <Box {...args} backgroundColor="gray" mt="md" textColor="alert">
+        <p>Color Alert</p>
+      </Box>
+      <Box {...args} mt="xl" textType="body">
+        <p>Body size & leading default value</p>
+      </Box>
+
+      <Box {...args} mt="xl" textType="body" textSize="sm" textLeading="default">
+        <p>Body Small Default</p>
+      </Box>
+      <Box {...args} mt="md" textType="body" textSize="sm" textLeading="narrow">
+        <p>Body Small Narrow</p>
+      </Box>
+      <Box {...args} mt="md" textType="body" textSize="sm" textLeading="tight">
+        <p>Body Small Tight</p>
+      </Box>
+
+      <Box {...args} mt="xl" textType="body" textSize="md" textLeading="default">
+        <p>Body Medium Default</p>
+      </Box>
+      <Box {...args} mt="md" textType="body" textSize="md" textLeading="narrow">
+        <p>Body Medium Narrow</p>
+      </Box>
+      <Box {...args} mt="md" textType="body" textSize="md" textLeading="tight">
+        <p>Body Medium Tight</p>
+      </Box>
+
+      <Box {...args} mt="xl" textType="body" textSize="lg" textLeading="default">
+        <p>Body Large Default</p>
+      </Box>
+      <Box {...args} mt="md" textType="body" textSize="lg" textLeading="narrow">
+        <p>Body Large Narrow</p>
+      </Box>
+      <Box {...args} mt="md" textType="body" textSize="lg" textLeading="tight">
+        <p>Body Large Tight</p>
+      </Box>
+
+      <Box {...args} mt="xl" textType="note">
+        <p>Note size & leading default value</p>
+      </Box>
+
+      <Box {...args} mt="xl" textType="note" textSize="sm" textLeading="default">
+        <p>Note Small Default</p>
+      </Box>
+      <Box {...args} mt="md" textType="note" textSize="sm" textLeading="narrow">
+        <p>Note Small Narrow</p>
+      </Box>
+      <Box {...args} mt="md" textType="note" textSize="sm" textLeading="tight">
+        <p>Note Small Tight</p>
+      </Box>
+
+      <Box {...args} mt="xl" textType="note" textSize="md" textLeading="default">
+        <p>Note Medium Default</p>
+      </Box>
+      <Box {...args} mt="md" textType="note" textSize="md" textLeading="narrow">
+        <p>Note Medium Narrow</p>
+      </Box>
+      <Box {...args} mt="md" textType="note" textSize="md" textLeading="tight">
+        <p>Note Medium Tight</p>
+      </Box>
+
+      <Box {...args} mt="xl" textType="note" textSize="lg" textLeading="default">
+        <p>Note Large Default</p>
+      </Box>
+      <Box {...args} mt="md" textType="note" textSize="lg" textLeading="narrow">
+        <p>Note Large Narrow</p>
+      </Box>
+      <Box {...args} mt="md" textType="note" textSize="lg" textLeading="tight">
+        <p>Note Large Tight</p>
+      </Box>
+
+      <Box {...defaultArgs} mt="xl" textAlign="left">
+        <p>Text Left</p>
+      </Box>
+      <Box {...defaultArgs} mt="md" textAlign="center">
+        <p>Text Center</p>
+      </Box>
+      <Box {...defaultArgs} mt="md" textAlign="right">
+        <p>Text Right</p>
+      </Box>
+      <div style={{
+      textAlign: 'center'
+    }}>
+        <Box {...defaultArgs} mt="md">
+          <p>Inherit(undefined)</p>
+        </Box>
+      </div>
+    </div>,
+  args: defaultArgs
+}`,...(_=(R=p.parameters)==null?void 0:R.docs)==null?void 0:_.source}}};var H,I,V;c.parameters={...c.parameters,docs:{...(H=c.parameters)==null?void 0:H.docs,source:{originalSource:`{
+  render: args => <Box {...args} />,
+  args: {
+    ['data-test-id']: 'some-test',
+    ...defaultArgs
+  }
+}`,...(V=(I=c.parameters)==null?void 0:I.docs)==null?void 0:V.source}}};const Z=["Default","BackgroundColor","Padding","Margin","Radius","Border","Width","AsSection","TextVariations","CustomDataAttribute"];export{i as AsSection,o as BackgroundColor,a as Border,c as CustomDataAttribute,d as Default,l as Margin,x as Padding,m as Radius,p as TextVariations,s as Width,Z as __namedExportsOrder,Y as default};
---
+++ assets/Input.stories-864e559b.js
@@ -0,0 +1,100 @@
+import{j as e}from"./jsx-runtime-69eee039.js";import{r as t}from"./index-7c191284.js";import{I as n}from"./Input-53d07cda.js";import{S as H}from"./Stack-79a82653.js";import"./clsx-1229b3e0.js";import"./style-e9a2251f.js";const X={title:"Form/Input",component:n},r={render:()=>{const[l,i]=t.useState("Lorem ipsum"),m=t.useCallback(v=>{i(v.target.value)},[]);return e.jsxs(H,{spacing:"md",children:[e.jsx(n,{value:l,onChange:m}),e.jsxs("dl",{children:[e.jsx("dt",{children:"Value"}),e.jsx("dd",{children:l})]})]})}},u={render:()=>{const[l,i]=t.useState("lorem ipsum"),[m,v]=t.useState("test@example.com"),[F,M]=t.useState("abcd1234"),[f,L]=t.useState("000-0000-0000"),[D,N]=t.useState("http://www.example.com"),[P,U]=t.useState("10"),_=t.useCallback(a=>{i(a.target.value)},[]),z=t.useCallback(a=>{v(a.target.value)},[]),A=t.useCallback(a=>{M(a.target.value)},[]),B=t.useCallback(a=>{L(a.target.value)},[]),O=t.useCallback(a=>{N(a.target.value)},[]),R=t.useCallback(a=>{U(a.target.value)},[]),s={display:"inline-block",paddingBottom:"8px",fontSize:"13px"};return e.jsxs(H,{spacing:"md",children:[e.jsxs("div",{children:[e.jsx("label",{htmlFor:"tv-text",style:s,children:"text"}),e.jsx(n,{id:"tv-text",value:l,onChange:_})]}),e.jsxs("div",{children:[e.jsx("label",{htmlFor:"tv-mail",style:s,children:"mail"}),e.jsx(n,{type:"email",id:"tv-mail",value:m,onChange:z})]}),e.jsxs("div",{children:[e.jsx("label",{htmlFor:"tv-password",style:s,children:"password"}),e.jsx(n,{type:"password",id:"tv-password",value:F,onChange:A})]}),e.jsxs("div",{children:[e.jsx("label",{htmlFor:"tv-tel",style:s,children:"tel"}),e.jsx(n,{type:"tel",id:"tv-tel",value:f,onChange:B})]}),e.jsxs("div",{children:[e.jsx("label",{htmlFor:"tv-url",style:s,children:"url"}),e.jsx(n,{type:"url",id:"tv-url",value:D,onChange:O})]}),e.jsxs("div",{children:[e.jsx("label",{htmlFor:"tv-number",style:s,children:"number"}),e.jsx(n,{type:"number",id:"tv-number",value:P,onChange:R})]})]})}},o={render:()=>e.jsx(n,{value:"lorem ipsum"})},d={render:()=>e.jsx(n,{value:"wrong value",isInvalid:!0})},c={args:{"data-test-id":"input-custom-attribute"},render:l=>e.jsx(n,{...l})};var p,b,g;r.parameters={...r.parameters,docs:{...(p=r.parameters)==null?void 0:p.docs,source:{originalSource:`{
+  render: () => {
+    const [value, setValue] = useState('Lorem ipsum');
+    const onChange: ChangeEventHandler<HTMLInputElement> = useCallback(event => {
+      setValue(event.target.value);
+    }, []);
+    return <Stack spacing="md">
+        <Input value={value} onChange={onChange} />
+
+        <dl>
+          <dt>Value</dt>
+          <dd>{value}</dd>
+        </dl>
+      </Stack>;
+  }
+}`,...(g=(b=r.parameters)==null?void 0:b.docs)==null?void 0:g.source}}};var h,x,V;u.parameters={...u.parameters,docs:{...(h=u.parameters)==null?void 0:h.docs,source:{originalSource:`{
+  render: () => {
+    const [textValue, setTextValue] = useState('lorem ipsum');
+    const [mailValue, setMailValue] = useState('test@example.com');
+    const [passwordValue, setPasswordValue] = useState('abcd1234');
+    const [telValue, setTelValue] = useState('000-0000-0000');
+    const [urlValue, setUrlValue] = useState('http://www.example.com');
+    const [numberValue, setNumberValue] = useState('10');
+    const onChangeText: ChangeEventHandler<HTMLInputElement> = useCallback(event => {
+      setTextValue(event.target.value);
+    }, []);
+    const onChangeMail: ChangeEventHandler<HTMLInputElement> = useCallback(event => {
+      setMailValue(event.target.value);
+    }, []);
+    const onPasswordValue: ChangeEventHandler<HTMLInputElement> = useCallback(event => {
+      setPasswordValue(event.target.value);
+    }, []);
+    const onTelValue: ChangeEventHandler<HTMLInputElement> = useCallback(event => {
+      setTelValue(event.target.value);
+    }, []);
+    const onUrlValue: ChangeEventHandler<HTMLInputElement> = useCallback(event => {
+      setUrlValue(event.target.value);
+    }, []);
+    const onNumberValue: ChangeEventHandler<HTMLInputElement> = useCallback(event => {
+      setNumberValue(event.target.value);
+    }, []);
+    const labelStyle = {
+      display: 'inline-block',
+      paddingBottom: '8px',
+      fontSize: '13px'
+    };
+    return <Stack spacing="md">
+        <div>
+          <label htmlFor="tv-text" style={labelStyle}>
+            text
+          </label>
+          <Input id="tv-text" value={textValue} onChange={onChangeText} />
+        </div>
+
+        <div>
+          <label htmlFor="tv-mail" style={labelStyle}>
+            mail
+          </label>
+          <Input type="email" id="tv-mail" value={mailValue} onChange={onChangeMail} />
+        </div>
+
+        <div>
+          <label htmlFor="tv-password" style={labelStyle}>
+            password
+          </label>
+          <Input type="password" id="tv-password" value={passwordValue} onChange={onPasswordValue} />
+        </div>
+
+        <div>
+          <label htmlFor="tv-tel" style={labelStyle}>
+            tel
+          </label>
+          <Input type="tel" id="tv-tel" value={telValue} onChange={onTelValue} />
+        </div>
+
+        <div>
+          <label htmlFor="tv-url" style={labelStyle}>
+            url
+          </label>
+          <Input type="url" id="tv-url" value={urlValue} onChange={onUrlValue} />
+        </div>
+
+        <div>
+          <label htmlFor="tv-number" style={labelStyle}>
+            number
+          </label>
+          <Input type="number" id="tv-number" value={numberValue} onChange={onNumberValue} />
+        </div>
+      </Stack>;
+  }
+}`,...(V=(x=u.parameters)==null?void 0:x.docs)==null?void 0:V.source}}};var C,S,y;o.parameters={...o.parameters,docs:{...(C=o.parameters)==null?void 0:C.docs,source:{originalSource:`{
+  render: () => <Input value="lorem ipsum" />
+}`,...(y=(S=o.parameters)==null?void 0:S.docs)==null?void 0:y.source}}};var j,I,w;d.parameters={...d.parameters,docs:{...(j=d.parameters)==null?void 0:j.docs,source:{originalSource:`{
+  render: () => <Input value="wrong value" isInvalid />
+}`,...(w=(I=d.parameters)==null?void 0:I.docs)==null?void 0:w.source}}};var k,T,E;c.parameters={...c.parameters,docs:{...(k=c.parameters)==null?void 0:k.docs,source:{originalSource:`{
+  args: {
+    'data-test-id': 'input-custom-attribute'
+  },
+  render: args => <Input {...args} />
+}`,...(E=(T=c.parameters)==null?void 0:T.docs)==null?void 0:E.source}}};const Y=["Default","Types","Disabled","IsInvalid","CustomDataAttribute"];export{c as CustomDataAttribute,r as Default,o as Disabled,d as IsInvalid,u as Types,Y as __namedExportsOrder,X as default};
