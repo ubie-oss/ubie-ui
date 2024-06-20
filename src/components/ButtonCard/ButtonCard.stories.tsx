b406ed98a39893053e5ad0be36e2d9f966bc8c8d
hiroki.yokouchi <hiroki.yokouchi@dr-ubie.com>
2024-06-20 16:55:18 +0900

    - Added side-by-side Story - Allows testing of click events

--- src/components/ButtonCard/ButtonCard.stories.tsx
+++ src/components/ButtonCard/ButtonCard.stories.tsx
@@ -1,25 +1,42 @@
+import { action } from '@storybook/addon-actions';
 import { Meta, StoryObj } from '@storybook/react';
 import { ButtonCard } from './ButtonCard';
+import { Flex } from '../Flex/Flex';
 
 export default {
   title: 'Form/ButtonCard',
   component: ButtonCard,
   args: {
+    onClick: action('on click'),
     children: '自分',
   },
 } satisfies Meta<typeof ButtonCard>;
 
 const defaultArgs = {};
 
-export const Default: StoryObj<typeof ButtonCard> = {
+type Story = StoryObj<typeof ButtonCard>;
+
+export const Default: Story = {
   render: (args) => <ButtonCard {...args} />,
   args: defaultArgs,
 };
 
-export const Disabled: StoryObj<typeof ButtonCard> = {
+export const Disabled: Story = {
   render: (args) => <ButtonCard {...args} />,
   args: {
     ...defaultArgs,
     disabled: true,
+  },
+};
+
+export const Horizontal: Story = {
+  render: (args) => (
+    <Flex spacing="xs">
+      <ButtonCard {...args} />
+      <ButtonCard {...args} />
+    </Flex>
+  ),
+  args: {
+    ...defaultArgs,
   },
 };
