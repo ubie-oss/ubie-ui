d65efdc8f917f53e7847bb16dac2911fecacaa59
takanorip <takanorip@users.noreply.github.com>
2024-07-17 09:47:09 +0000

    deploy: 4be9764f837ea4f5978f18fae1322e36f93b2b23

---
+++ assets/Button.stories-2d2f64ee.js
@@ -0,0 +1,215 @@
+import{j as n}from"./jsx-runtime-69eee039.js";import{S as s,a as m}from"./TrashIcon-de957c6c.js";import{B as t}from"./Button-d76628fc.js";import{S as r}from"./Stack-3a809a50.js";import{I as i}from"./Icon-858d0d22.js";import"./index-7c191284.js";import"./clsx-1229b3e0.js";import"./VariantIcon-23f7151c.js";import"./style-1d55b7c3.js";import"./ArrowBDownIcon-86826e18.js";import"./ThumbUpOutlineIcon-17e2fa33.js";import"./UbieIcon-105662b4.js";import"./UnfoldMoreIcon-402a05cb.js";const $={title:"Button/Button",component:t},e={children:"ボタン",disabled:!1},l={render:()=>n.jsxs("div",{style:{display:"flex",flexDirection:"row",alignItems:"flex-start",gap:"32px"},children:[n.jsx(t,{...e}),n.jsx(t,{...e,variant:"secondary"}),n.jsx(t,{...e,variant:"accent"}),n.jsx(t,{...e,variant:"alert"}),n.jsx(t,{...e,variant:"text"}),n.jsx(t,{...e,variant:"textAlert"})]})},o={render:()=>n.jsxs("div",{style:{display:"flex",flexDirection:"row",alignItems:"flex-start",gap:"32px"},children:[n.jsx(t,{...e,size:"small",children:"Small"}),n.jsx(t,{...e,size:"medium",children:"Medium"}),n.jsx(t,{...e,children:"Large"})]}),args:e},d={render:()=>n.jsxs(r,{spacing:"lg",children:[n.jsxs(r,{spacing:"lg",as:"dl",children:[n.jsx("dt",{style:{fontWeight:"bold"},children:"Default Position"}),n.jsx("dd",{style:{margin:0},children:n.jsxs("div",{style:{display:"flex",flexDirection:"row",alignItems:"flex-start",gap:"32px"},children:[n.jsx(t,{icon:n.jsx(i,{icon:"UbieIcon"}),...e}),n.jsx(t,{icon:n.jsx(i,{icon:"UbieIcon"}),...e,variant:"secondary"}),n.jsx(t,{icon:n.jsx(i,{icon:"UbieIcon"}),...e,variant:"accent"}),n.jsx(t,{icon:n.jsx(i,{icon:"UbieIcon"}),...e,variant:"alert"}),n.jsx(t,{icon:n.jsx(i,{icon:"UbieIcon"}),...e,variant:"text"}),n.jsx(t,{icon:n.jsx(i,{icon:"TrashIcon"}),...e,variant:"textAlert"})]})})]}),n.jsxs(r,{spacing:"lg",as:"dl",children:[n.jsx("dt",{style:{fontWeight:"bold"},children:"Position Suffix"}),n.jsx("dd",{style:{margin:0},children:n.jsxs("div",{style:{display:"flex",flexDirection:"row",alignItems:"flex-start",gap:"32px"},children:[n.jsx(t,{suffixIcon:n.jsx(s,{}),...e}),n.jsx(t,{suffixIcon:n.jsx(s,{}),...e,variant:"secondary"}),n.jsx(t,{suffixIcon:n.jsx(s,{}),...e,variant:"accent"}),n.jsx(t,{suffixIcon:n.jsx(s,{}),...e,variant:"alert"}),n.jsx(t,{suffixIcon:n.jsx(s,{}),...e,variant:"text"}),n.jsx(t,{suffixIcon:n.jsx(m,{}),...e,variant:"textAlert"})]})})]}),n.jsxs(r,{spacing:"lg",as:"dl",children:[n.jsx("dt",{style:{fontWeight:"bold"},children:"Position Fixed"}),n.jsx("dd",{style:{margin:0},children:n.jsxs("div",{style:{display:"flex",flexDirection:"row",alignItems:"flex-start",gap:"32px"},children:[n.jsx(t,{fixedIcon:n.jsx(s,{}),...e}),n.jsx(t,{fixedIcon:n.jsx(s,{}),...e,variant:"secondary"}),n.jsx(t,{fixedIcon:n.jsx(s,{}),...e,variant:"accent"}),n.jsx(t,{fixedIcon:n.jsx(s,{}),...e,variant:"alert"}),n.jsx(t,{fixedIcon:n.jsx(s,{}),...e,variant:"text"}),n.jsx(t,{fixedIcon:n.jsx(m,{}),...e,variant:"textAlert"})]})})]})]}),args:e},c={render:a=>n.jsxs("div",{style:{display:"flex",flexDirection:"column",gap:"32px"},children:[n.jsx("div",{children:n.jsx(t,{...a,size:"small",variant:"secondary"})}),n.jsx("div",{children:n.jsx(t,{...a,size:"medium",variant:"secondary"})}),n.jsx("div",{children:n.jsx(t,{...a,size:"large",variant:"secondary"})})]}),args:{...e,block:!0}},x={render:()=>n.jsxs("div",{style:{display:"flex",flexDirection:"row",alignItems:"flex-start",gap:"32px"},children:[n.jsx(t,{...e,disabled:!0}),n.jsx(t,{...e,variant:"secondary",disabled:!0}),n.jsx(t,{...e,variant:"accent",disabled:!0}),n.jsx(t,{...e,variant:"alert",disabled:!0}),n.jsx(t,{...e,variant:"text",disabled:!0}),n.jsx(t,{...e,variant:"textAlert",disabled:!0})]}),args:e},u={args:{...e,children:"OK",loading:!0},render:a=>n.jsxs(r,{spacing:"md",children:[n.jsxs("div",{style:{display:"flex",flexDirection:"row",alignItems:"flex-start",gap:"32px"},children:[n.jsx(t,{...a}),n.jsx(t,{...a,variant:"secondary"}),n.jsx(t,{...a,variant:"accent"}),n.jsx(t,{...a,variant:"alert"}),n.jsx(t,{...a,variant:"text"}),n.jsx(t,{...a,variant:"textAlert"})]}),n.jsxs("div",{style:{display:"flex",flexDirection:"row",alignItems:"flex-start",gap:"32px"},children:[n.jsx(t,{...a,size:"medium"}),n.jsx(t,{...a,variant:"secondary",size:"medium"}),n.jsx(t,{...a,variant:"accent",size:"medium"}),n.jsx(t,{...a,variant:"alert",size:"medium"}),n.jsx(t,{...a,variant:"text",size:"medium"}),n.jsx(t,{...a,variant:"textAlert",size:"medium"})]}),n.jsxs("div",{style:{display:"flex",flexDirection:"row",alignItems:"flex-start",gap:"32px"},children:[n.jsx(t,{...a,size:"small"}),n.jsx(t,{...a,variant:"secondary",size:"small"}),n.jsx(t,{...a,variant:"accent",size:"small"}),n.jsx(t,{...a,variant:"alert",size:"small"}),n.jsx(t,{...a,variant:"text",size:"small"}),n.jsx(t,{...a,variant:"textAlert",size:"small"})]}),n.jsxs("div",{style:{display:"flex",flexDirection:"row",alignItems:"flex-start",gap:"32px"},children:[n.jsx(t,{...a,icon:n.jsx(i,{icon:"UbieIcon"}),size:"medium",loadingLabel:"ラベル変更",children:"次のページへ"}),n.jsx(t,{...a,variant:"secondary",size:"medium",disabled:!0})]})]})},g={render:()=>n.jsx("div",{style:{backgroundColor:"var(--color-background-gray)",width:"fit-content",overflow:"hidden"},children:n.jsx(t,{...e,mt:"lg",mr:"lg",mb:"lg",ml:"lg"})})},f={render:a=>n.jsx(t,{...a}),args:{"data-test-id":"some-test",...e}};var v,p,j;l.parameters={...l.parameters,docs:{...(v=l.parameters)==null?void 0:v.docs,source:{originalSource:`{
+  render: () => <div style={{
+    display: 'flex',
+    flexDirection: 'row',
+    alignItems: 'flex-start',
+    gap: '32px'
+  }}>
+      <Button {...defaultArgs} />
+      <Button {...defaultArgs} variant="secondary" />
+      <Button {...defaultArgs} variant="accent" />
+      <Button {...defaultArgs} variant="alert" />
+      <Button {...defaultArgs} variant="text" />
+      <Button {...defaultArgs} variant="textAlert" />
+    </div>
+}`,...(j=(p=l.parameters)==null?void 0:p.docs)==null?void 0:j.source}}};var y,B,I;o.parameters={...o.parameters,docs:{...(y=o.parameters)==null?void 0:y.docs,source:{originalSource:`{
+  render: () => <div style={{
+    display: 'flex',
+    flexDirection: 'row',
+    alignItems: 'flex-start',
+    gap: '32px'
+  }}>
+      <Button {...defaultArgs} size="small">
+        Small
+      </Button>
+
+      <Button {...defaultArgs} size="medium">
+        Medium
+      </Button>
+
+      <Button {...defaultArgs}>Large</Button>
+    </div>,
+  args: defaultArgs
+}`,...(I=(B=o.parameters)==null?void 0:B.docs)==null?void 0:I.source}}};var A,h,b;d.parameters={...d.parameters,docs:{...(A=d.parameters)==null?void 0:A.docs,source:{originalSource:`{
+  render: () => <Stack spacing="lg">
+      <Stack spacing="lg" as="dl">
+        <dt style={{
+        fontWeight: 'bold'
+      }}>Default Position</dt>
+        <dd style={{
+        margin: 0
+      }}>
+          <div style={{
+          display: 'flex',
+          flexDirection: 'row',
+          alignItems: 'flex-start',
+          gap: '32px'
+        }}>
+            <Button icon={<Icon icon="UbieIcon" />} {...defaultArgs} />
+            <Button icon={<Icon icon="UbieIcon" />} {...defaultArgs} variant="secondary" />
+            <Button icon={<Icon icon="UbieIcon" />} {...defaultArgs} variant="accent" />
+            <Button icon={<Icon icon="UbieIcon" />} {...defaultArgs} variant="alert" />
+            <Button icon={<Icon icon="UbieIcon" />} {...defaultArgs} variant="text" />
+            <Button icon={<Icon icon="TrashIcon" />} {...defaultArgs} variant="textAlert" />
+          </div>
+        </dd>
+      </Stack>
+
+      <Stack spacing="lg" as="dl">
+        <dt style={{
+        fontWeight: 'bold'
+      }}>Position Suffix</dt>
+        <dd style={{
+        margin: 0
+      }}>
+          <div style={{
+          display: 'flex',
+          flexDirection: 'row',
+          alignItems: 'flex-start',
+          gap: '32px'
+        }}>
+            <Button suffixIcon={<BlankLinkIcon />} {...defaultArgs} />
+            <Button suffixIcon={<BlankLinkIcon />} {...defaultArgs} variant="secondary" />
+            <Button suffixIcon={<BlankLinkIcon />} {...defaultArgs} variant="accent" />
+            <Button suffixIcon={<BlankLinkIcon />} {...defaultArgs} variant="alert" />
+            <Button suffixIcon={<BlankLinkIcon />} {...defaultArgs} variant="text" />
+            <Button suffixIcon={<TrashIcon />} {...defaultArgs} variant="textAlert" />
+          </div>
+        </dd>
+      </Stack>
+
+      <Stack spacing="lg" as="dl">
+        <dt style={{
+        fontWeight: 'bold'
+      }}>Position Fixed</dt>
+        <dd style={{
+        margin: 0
+      }}>
+          <div style={{
+          display: 'flex',
+          flexDirection: 'row',
+          alignItems: 'flex-start',
+          gap: '32px'
+        }}>
+            <Button fixedIcon={<BlankLinkIcon />} {...defaultArgs} />
+            <Button fixedIcon={<BlankLinkIcon />} {...defaultArgs} variant="secondary" />
+            <Button fixedIcon={<BlankLinkIcon />} {...defaultArgs} variant="accent" />
+            <Button fixedIcon={<BlankLinkIcon />} {...defaultArgs} variant="alert" />
+            <Button fixedIcon={<BlankLinkIcon />} {...defaultArgs} variant="text" />
+            <Button fixedIcon={<TrashIcon />} {...defaultArgs} variant="textAlert" />
+          </div>
+        </dd>
+      </Stack>
+    </Stack>,
+  args: defaultArgs
+}`,...(b=(h=d.parameters)==null?void 0:h.docs)==null?void 0:b.source}}};var k,z,D;c.parameters={...c.parameters,docs:{...(k=c.parameters)==null?void 0:k.docs,source:{originalSource:`{
+  render: args => <div style={{
+    display: 'flex',
+    flexDirection: 'column',
+    gap: '32px'
+  }}>
+      <div>
+        <Button {...args} size="small" variant="secondary" />
+      </div>
+      <div>
+        <Button {...args} size="medium" variant="secondary" />
+      </div>
+      <div>
+        <Button {...args} size="large" variant="secondary" />
+      </div>
+    </div>,
+  args: {
+    ...defaultArgs,
+    block: true
+  }
+}`,...(D=(z=c.parameters)==null?void 0:z.docs)==null?void 0:D.source}}};var S,w,L;x.parameters={...x.parameters,docs:{...(S=x.parameters)==null?void 0:S.docs,source:{originalSource:`{
+  render: () => <div style={{
+    display: 'flex',
+    flexDirection: 'row',
+    alignItems: 'flex-start',
+    gap: '32px'
+  }}>
+      <Button {...defaultArgs} disabled />
+      <Button {...defaultArgs} variant="secondary" disabled />
+      <Button {...defaultArgs} variant="accent" disabled />
+      <Button {...defaultArgs} variant="alert" disabled />
+      <Button {...defaultArgs} variant="text" disabled />
+      <Button {...defaultArgs} variant="textAlert" disabled />
+    </div>,
+  args: defaultArgs
+}`,...(L=(w=x.parameters)==null?void 0:w.docs)==null?void 0:L.source}}};var U,W,P;u.parameters={...u.parameters,docs:{...(U=u.parameters)==null?void 0:U.docs,source:{originalSource:`{
+  args: {
+    ...defaultArgs,
+    children: 'OK',
+    loading: true
+  },
+  render: args => <Stack spacing="md">
+      <div style={{
+      display: 'flex',
+      flexDirection: 'row',
+      alignItems: 'flex-start',
+      gap: '32px'
+    }}>
+        <Button {...args} />
+        <Button {...args} variant="secondary" />
+        <Button {...args} variant="accent" />
+        <Button {...args} variant="alert" />
+        <Button {...args} variant="text" />
+        <Button {...args} variant="textAlert" />
+      </div>
+
+      <div style={{
+      display: 'flex',
+      flexDirection: 'row',
+      alignItems: 'flex-start',
+      gap: '32px'
+    }}>
+        <Button {...args} size="medium" />
+        <Button {...args} variant="secondary" size="medium" />
+        <Button {...args} variant="accent" size="medium" />
+        <Button {...args} variant="alert" size="medium" />
+        <Button {...args} variant="text" size="medium" />
+        <Button {...args} variant="textAlert" size="medium" />
+      </div>
+
+      <div style={{
+      display: 'flex',
+      flexDirection: 'row',
+      alignItems: 'flex-start',
+      gap: '32px'
+    }}>
+        <Button {...args} size="small" />
+        <Button {...args} variant="secondary" size="small" />
+        <Button {...args} variant="accent" size="small" />
+        <Button {...args} variant="alert" size="small" />
+        <Button {...args} variant="text" size="small" />
+        <Button {...args} variant="textAlert" size="small" />
+      </div>
+
+      <div style={{
+      display: 'flex',
+      flexDirection: 'row',
+      alignItems: 'flex-start',
+      gap: '32px'
+    }}>
+        <Button {...args} icon={<Icon icon="UbieIcon" />} size="medium" loadingLabel="ラベル変更">
+          次のページへ
+        </Button>
+        <Button {...args} variant="secondary" size="medium" disabled />
+      </div>
+    </Stack>
+}`,...(P=(W=u.parameters)==null?void 0:W.docs)==null?void 0:P.source}}};var C,E,T;g.parameters={...g.parameters,docs:{...(C=g.parameters)==null?void 0:C.docs,source:{originalSource:`{
+  render: () => <div style={{
+    backgroundColor: 'var(--color-background-gray)',
+    width: 'fit-content',
+    overflow: 'hidden'
+  }}>
+      <Button {...defaultArgs} mt="lg" mr="lg" mb="lg" ml="lg" />
+    </div>
+}`,...(T=(E=g.parameters)==null?void 0:E.docs)==null?void 0:T.source}}};var F,M,O;f.parameters={...f.parameters,docs:{...(F=f.parameters)==null?void 0:F.docs,source:{originalSource:`{
+  render: args => <Button {...args} />,
+  args: {
+    ['data-test-id']: 'some-test',
+    ...defaultArgs
+  }
+}`,...(O=(M=f.parameters)==null?void 0:M.docs)==null?void 0:O.source}}};const nn=["Default","Size","WithIcon","Block","Disabled","Loading","Margin","CustomDataAttribute"];export{c as Block,f as CustomDataAttribute,l as Default,x as Disabled,u as Loading,g as Margin,o as Size,d as WithIcon,nn as __namedExportsOrder,$ as default};
---
+++ assets/Input.stories-b0d1fa35.js
@@ -0,0 +1,110 @@
+import{j as e}from"./jsx-runtime-69eee039.js";import{r as t}from"./index-7c191284.js";import{I as a}from"./Input-43c992ed.js";import{S as U}from"./Stack-3a809a50.js";import"./clsx-1229b3e0.js";import"./style-1d55b7c3.js";const le={title:"Form/Input",component:a},d={render:()=>{const[l,r]=t.useState("Lorem ipsum"),u=t.useCallback(o=>{r(o.target.value)},[]);return e.jsxs(U,{spacing:"md",children:[e.jsx(a,{value:l,onChange:u}),e.jsxs("dl",{children:[e.jsx("dt",{children:"Value"}),e.jsx("dd",{children:l})]})]})}},c={render:()=>{const[l,r]=t.useState("lorem ipsum"),[u,o]=t.useState("test@example.com"),[q,R]=t.useState("abcd1234"),[_,z]=t.useState("000-0000-0000"),[A,B]=t.useState("http://www.example.com"),[O,G]=t.useState("10"),J=t.useCallback(n=>{r(n.target.value)},[]),K=t.useCallback(n=>{o(n.target.value)},[]),Q=t.useCallback(n=>{R(n.target.value)},[]),W=t.useCallback(n=>{z(n.target.value)},[]),X=t.useCallback(n=>{B(n.target.value)},[]),Y=t.useCallback(n=>{G(n.target.value)},[]),s={display:"inline-block",paddingBottom:"8px",fontSize:"13px"};return e.jsxs(U,{spacing:"md",children:[e.jsxs("div",{children:[e.jsx("label",{htmlFor:"tv-text",style:s,children:"text"}),e.jsx(a,{id:"tv-text",value:l,onChange:J})]}),e.jsxs("div",{children:[e.jsx("label",{htmlFor:"tv-mail",style:s,children:"mail"}),e.jsx(a,{type:"email",id:"tv-mail",value:u,onChange:K})]}),e.jsxs("div",{children:[e.jsx("label",{htmlFor:"tv-password",style:s,children:"password"}),e.jsx(a,{type:"password",id:"tv-password",value:q,onChange:Q})]}),e.jsxs("div",{children:[e.jsx("label",{htmlFor:"tv-tel",style:s,children:"tel"}),e.jsx(a,{type:"tel",id:"tv-tel",value:_,onChange:W})]}),e.jsxs("div",{children:[e.jsx("label",{htmlFor:"tv-url",style:s,children:"url"}),e.jsx(a,{type:"url",id:"tv-url",value:A,onChange:X})]}),e.jsxs("div",{children:[e.jsx("label",{htmlFor:"tv-number",style:s,children:"number"}),e.jsx(a,{type:"number",id:"tv-number",value:O,onChange:Y})]})]})}},i={render:()=>e.jsx(a,{value:"lorem ipsum",disabled:!0})},m={render:()=>e.jsx(a,{value:"wrong value",isInvalid:!0})},p={render:()=>{const[l,r]=t.useState(""),u=t.useCallback(o=>{r(o.target.value)},[]);return e.jsx(a,{required:!0,value:l,onChange:u})}},v={render:()=>e.jsx(a,{placeholder:"placeholder"})},h={args:{"data-test-id":"input-custom-attribute"},render:l=>e.jsx(a,{...l})};var b,g,V;d.parameters={...d.parameters,docs:{...(b=d.parameters)==null?void 0:b.docs,source:{originalSource:`{
+  render: () => {
+    const [value, setValue] = useState('Lorem ipsum');
+    const onChange: ChangeEventHandler<HTMLInputElement> = useCallback(event => {
+      setValue(event.target.value);
+    }, []);
+    return <Stack spacing="md">
+        <Input value={value} onChange={onChange} />
+
+        <dl>
+          <dt>Value</dt>
+          <dd>{value}</dd>
+        </dl>
+      </Stack>;
+  }
+}`,...(V=(g=d.parameters)==null?void 0:g.docs)==null?void 0:V.source}}};var x,C,S;c.parameters={...c.parameters,docs:{...(x=c.parameters)==null?void 0:x.docs,source:{originalSource:`{
+  render: () => {
+    const [textValue, setTextValue] = useState('lorem ipsum');
+    const [mailValue, setMailValue] = useState('test@example.com');
+    const [passwordValue, setPasswordValue] = useState('abcd1234');
+    const [telValue, setTelValue] = useState('000-0000-0000');
+    const [urlValue, setUrlValue] = useState('http://www.example.com');
+    const [numberValue, setNumberValue] = useState('10');
+    const onChangeText: ChangeEventHandler<HTMLInputElement> = useCallback(event => {
+      setTextValue(event.target.value);
+    }, []);
+    const onChangeMail: ChangeEventHandler<HTMLInputElement> = useCallback(event => {
+      setMailValue(event.target.value);
+    }, []);
+    const onPasswordValue: ChangeEventHandler<HTMLInputElement> = useCallback(event => {
+      setPasswordValue(event.target.value);
+    }, []);
+    const onTelValue: ChangeEventHandler<HTMLInputElement> = useCallback(event => {
+      setTelValue(event.target.value);
+    }, []);
+    const onUrlValue: ChangeEventHandler<HTMLInputElement> = useCallback(event => {
+      setUrlValue(event.target.value);
+    }, []);
+    const onNumberValue: ChangeEventHandler<HTMLInputElement> = useCallback(event => {
+      setNumberValue(event.target.value);
+    }, []);
+    const labelStyle = {
+      display: 'inline-block',
+      paddingBottom: '8px',
+      fontSize: '13px'
+    };
+    return <Stack spacing="md">
+        <div>
+          <label htmlFor="tv-text" style={labelStyle}>
+            text
+          </label>
+          <Input id="tv-text" value={textValue} onChange={onChangeText} />
+        </div>
+
+        <div>
+          <label htmlFor="tv-mail" style={labelStyle}>
+            mail
+          </label>
+          <Input type="email" id="tv-mail" value={mailValue} onChange={onChangeMail} />
+        </div>
+
+        <div>
+          <label htmlFor="tv-password" style={labelStyle}>
+            password
+          </label>
+          <Input type="password" id="tv-password" value={passwordValue} onChange={onPasswordValue} />
+        </div>
+
+        <div>
+          <label htmlFor="tv-tel" style={labelStyle}>
+            tel
+          </label>
+          <Input type="tel" id="tv-tel" value={telValue} onChange={onTelValue} />
+        </div>
+
+        <div>
+          <label htmlFor="tv-url" style={labelStyle}>
+            url
+          </label>
+          <Input type="url" id="tv-url" value={urlValue} onChange={onUrlValue} />
+        </div>
+
+        <div>
+          <label htmlFor="tv-number" style={labelStyle}>
+            number
+          </label>
+          <Input type="number" id="tv-number" value={numberValue} onChange={onNumberValue} />
+        </div>
+      </Stack>;
+  }
+}`,...(S=(C=c.parameters)==null?void 0:C.docs)==null?void 0:S.source}}};var y,j,I;i.parameters={...i.parameters,docs:{...(y=i.parameters)==null?void 0:y.docs,source:{originalSource:`{
+  render: () => <Input value="lorem ipsum" disabled />
+}`,...(I=(j=i.parameters)==null?void 0:j.docs)==null?void 0:I.source}}};var w,k,T;m.parameters={...m.parameters,docs:{...(w=m.parameters)==null?void 0:w.docs,source:{originalSource:`{
+  render: () => <Input value="wrong value" isInvalid />
+}`,...(T=(k=m.parameters)==null?void 0:k.docs)==null?void 0:T.source}}};var E,H,M;p.parameters={...p.parameters,docs:{...(E=p.parameters)==null?void 0:E.docs,source:{originalSource:`{
+  render: () => {
+    const [value, setValue] = useState('');
+    const onChangeValue: ChangeEventHandler<HTMLInputElement> = useCallback(event => {
+      setValue(event.target.value);
+    }, []);
+    return <Input required value={value} onChange={onChangeValue} />;
+  }
+}`,...(M=(H=p.parameters)==null?void 0:H.docs)==null?void 0:M.source}}};var F,L,f;v.parameters={...v.parameters,docs:{...(F=v.parameters)==null?void 0:F.docs,source:{originalSource:`{
+  render: () => <Input placeholder="placeholder" />
+}`,...(f=(L=v.parameters)==null?void 0:L.docs)==null?void 0:f.source}}};var P,D,N;h.parameters={...h.parameters,docs:{...(P=h.parameters)==null?void 0:P.docs,source:{originalSource:`{
+  args: {
+    'data-test-id': 'input-custom-attribute'
+  },
+  render: args => <Input {...args} />
+}`,...(N=(D=h.parameters)==null?void 0:D.docs)==null?void 0:N.source}}};const se=["Default","Types","Disabled","IsInvalid","Required","Placeholder","CustomDataAttribute"];export{h as CustomDataAttribute,d as Default,i as Disabled,m as IsInvalid,v as Placeholder,p as Required,c as Types,se as __namedExportsOrder,le as default};
