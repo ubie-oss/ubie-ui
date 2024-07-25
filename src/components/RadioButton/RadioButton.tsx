b4e5a6bb3de636ea63d1392d31af30a313957697
hiroki.yokouchi <hiroki.yokouchi@dr-ubie.com>
2024-07-25 23:40:37 +0900

    Standardized to ComponentPropsWithRef

--- src/components/RadioButton/RadioButton.tsx
+++ src/components/RadioButton/RadioButton.tsx
@@ -1,33 +1,25 @@
 'use client';
 
 import clsx from 'clsx';
-import { forwardRef, type InputHTMLAttributes } from 'react';
+import { type ComponentPropsWithRef, forwardRef, type ReactNode } from 'react';
 import styles from './RadioButton.module.css';
-import { CustomDataAttributeProps } from '../../types/attributes';
 
-type RadioProps = Required<Pick<InputHTMLAttributes<HTMLInputElement>, 'onChange' | 'checked'>> &
-  Omit<InputHTMLAttributes<HTMLInputElement>, 'size' | 'children' | 'value' | 'name'>;
-
-type Props = {
-  /**
-   * グループ化（排他制御）したい要素には同じ名前をつける
-   */
-  name: string;
-  /**
-   * 選択時のコールバックで渡される値
-   */
-  value: string | number;
+type RadioButtonProps = {
   /**
    * ラベルに表示されるテキストまたは要素
    */
-  children: InputHTMLAttributes<HTMLInputElement>['children'];
+  children: ReactNode;
   /**
    * サイズ
    * @default medium
    */
   size?: 'medium' | 'small';
-} & RadioProps &
-  CustomDataAttributeProps;
+};
+
+type InputProps = Required<Pick<ComponentPropsWithRef<'input'>, 'onChange' | 'checked' | 'value'>> &
+  Omit<ComponentPropsWithRef<'input'>, keyof RadioButtonProps | 'onChange' | 'checked' | 'value'>;
+
+type Props = RadioButtonProps & InputProps;
 
 export const RadioButton = forwardRef<HTMLInputElement, Props>(
   ({ size = 'medium', checked, onChange, value, name, children, ...otherProps }, ref) => {
