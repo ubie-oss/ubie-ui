d0868884ba81cb93d3b4cd67c459e68e6aaad5ac
takanorip <takanorip@users.noreply.github.com>
2024-07-26 07:47:47 +0000

    deploy: be1abb66fc445a381e6b81cab4905816fdbbf50c

---
+++ assets/Stack.stories-d52c0272.js
@@ -0,0 +1,190 @@
+import{j as n}from"./jsx-runtime-69eee039.js";import{S as e}from"./Stack-66f086af.js";import{B as L}from"./Box-7290ccc8.js";import"./index-7c191284.js";import"./clsx-1229b3e0.js";import"./style-1d55b7c3.js";const G={component:e},s={spacing:"md"},a={render:r=>n.jsxs(e,{...r,children:[n.jsx("p",{style:{margin:0},children:"テスト"}),n.jsx("p",{style:{margin:0},children:"テスト"}),n.jsx("p",{style:{margin:0},children:"テスト"}),n.jsx("p",{style:{margin:0},children:"テスト"})]}),args:s},t={render:()=>n.jsxs(e,{spacing:"xl",children:[n.jsxs(e,{spacing:"lg",children:[n.jsx("p",{style:{margin:0,background:"#DDD"},children:"テスト"}),n.jsx("p",{style:{margin:0,background:"#DDD"},children:"テスト"}),n.jsx("p",{style:{margin:0,background:"#DDD"},children:"テスト"})]}),n.jsxs(e,{spacing:"lg",children:[n.jsx("p",{style:{margin:0,background:"#DDD"},children:"テスト"}),n.jsx("p",{style:{margin:0,background:"#DDD"},children:"テスト"}),n.jsxs(e,{spacing:"xs",children:[n.jsx("p",{style:{margin:0,background:"#DDD"},children:"テスト"}),n.jsx("p",{style:{margin:0,background:"#DDD"},children:"テスト"}),n.jsx("p",{style:{margin:0,background:"#DDD"},children:"テスト"})]}),n.jsxs(e,{children:[n.jsx("p",{style:{margin:0,background:"#DDD"},children:"テスト"}),n.jsx("p",{style:{margin:0,background:"#DDD"},children:"テスト"}),n.jsx("p",{style:{margin:0,background:"#DDD"},children:"テスト"})]})]})]})},i={render:r=>n.jsxs(e,{...r,as:"ul",children:[n.jsx("li",{children:"テスト"}),n.jsx("li",{children:"テスト"}),n.jsx("li",{children:"テスト"}),n.jsx("li",{children:"テスト"})]}),args:s},l={render:r=>n.jsxs(e,{...r,alignItems:"flex-end",children:[n.jsx("p",{style:{margin:0},children:"テスト"}),n.jsx("p",{style:{margin:0},children:"テスト"}),n.jsx("p",{style:{margin:0},children:"テスト"}),n.jsx("p",{style:{margin:0},children:"テスト"})]}),args:s},p={render:r=>n.jsxs(e,{...r,mt:"lg",mr:"lg",mb:"lg",ml:"lg",pt:"xxl",pr:"xxl",pb:"xxl",pl:"xxl",children:[n.jsx("p",{style:{margin:0},children:"Text"}),n.jsx("p",{style:{margin:0},children:"Text"}),n.jsx("p",{style:{margin:0},children:"Text"}),n.jsx("p",{style:{margin:0},children:"Text"})]}),args:s},g={render:r=>n.jsxs(e,{...r,as:n.jsx(L,{radius:"md",backgroundColor:"primary",pt:"lg",pr:"lg",pb:"lg",pl:"lg"}),children:[n.jsx("p",{style:{margin:0},children:"Text"}),n.jsx("p",{style:{margin:0},children:"Text"}),n.jsx("p",{style:{margin:0},children:"Text"}),n.jsx("p",{style:{margin:0},children:"Text"})]}),args:s},c={render:r=>n.jsxs(e,{...r,children:[n.jsx("p",{children:"This is a paragraph."}),n.jsx("p",{children:"This is a paragraph."})]}),args:{...s,"data-test-id":"custom-attribute"}},d={render:r=>n.jsxs(n.Fragment,{children:[n.jsxs(e,{...r,width:"100%",children:[n.jsx("p",{style:{margin:0},children:"width: 100%"}),n.jsx("p",{style:{margin:0},children:"Text"}),n.jsx("p",{style:{margin:0},children:"Text"}),n.jsx("p",{style:{margin:0},children:"Text"})]}),n.jsx("br",{}),n.jsxs(e,{...r,minWidth:"500px",children:[n.jsx("p",{style:{margin:0},children:"min-width: 500px"}),n.jsx("p",{style:{margin:0},children:"Text"}),n.jsx("p",{style:{margin:0},children:"Text"}),n.jsx("p",{style:{margin:0},children:"Text"})]}),n.jsx("br",{}),n.jsxs(e,{...r,maxWidth:"500px",children:[n.jsx("p",{style:{margin:0},children:"max-width: 500px"}),n.jsx("p",{style:{margin:0},children:"Text"}),n.jsx("p",{style:{margin:0},children:"Text"}),n.jsx("p",{style:{margin:0},children:"Text"})]})]}),args:s};var m,u,x;a.parameters={...a.parameters,docs:{...(m=a.parameters)==null?void 0:m.docs,source:{originalSource:`{
+  render: args => <Stack {...args}>
+      <p style={{
+      margin: 0
+    }}>テスト</p>
+      <p style={{
+      margin: 0
+    }}>テスト</p>
+      <p style={{
+      margin: 0
+    }}>テスト</p>
+      <p style={{
+      margin: 0
+    }}>テスト</p>
+    </Stack>,
+  args: defaultArgs
+}`,...(x=(u=a.parameters)==null?void 0:u.docs)==null?void 0:x.source}}};var o,h,y;t.parameters={...t.parameters,docs:{...(o=t.parameters)==null?void 0:o.docs,source:{originalSource:`{
+  render: () => <Stack spacing="xl">
+      <Stack spacing="lg">
+        <p style={{
+        margin: 0,
+        background: '#DDD'
+      }}>テスト</p>
+        <p style={{
+        margin: 0,
+        background: '#DDD'
+      }}>テスト</p>
+        <p style={{
+        margin: 0,
+        background: '#DDD'
+      }}>テスト</p>
+      </Stack>
+
+      <Stack spacing="lg">
+        <p style={{
+        margin: 0,
+        background: '#DDD'
+      }}>テスト</p>
+        <p style={{
+        margin: 0,
+        background: '#DDD'
+      }}>テスト</p>
+        <Stack spacing="xs">
+          <p style={{
+          margin: 0,
+          background: '#DDD'
+        }}>テスト</p>
+          <p style={{
+          margin: 0,
+          background: '#DDD'
+        }}>テスト</p>
+          <p style={{
+          margin: 0,
+          background: '#DDD'
+        }}>テスト</p>
+        </Stack>
+        <Stack>
+          <p style={{
+          margin: 0,
+          background: '#DDD'
+        }}>テスト</p>
+          <p style={{
+          margin: 0,
+          background: '#DDD'
+        }}>テスト</p>
+          <p style={{
+          margin: 0,
+          background: '#DDD'
+        }}>テスト</p>
+        </Stack>
+      </Stack>
+    </Stack>
+}`,...(y=(h=t.parameters)==null?void 0:h.docs)==null?void 0:y.source}}};var D,j,k;i.parameters={...i.parameters,docs:{...(D=i.parameters)==null?void 0:D.docs,source:{originalSource:`{
+  render: args => <Stack {...args} as="ul">
+      <li>テスト</li>
+      <li>テスト</li>
+      <li>テスト</li>
+      <li>テスト</li>
+    </Stack>,
+  args: defaultArgs
+}`,...(k=(j=i.parameters)==null?void 0:j.docs)==null?void 0:k.source}}};var C,T,S;l.parameters={...l.parameters,docs:{...(C=l.parameters)==null?void 0:C.docs,source:{originalSource:`{
+  render: args => <Stack {...args} alignItems="flex-end">
+      <p style={{
+      margin: 0
+    }}>テスト</p>
+      <p style={{
+      margin: 0
+    }}>テスト</p>
+      <p style={{
+      margin: 0
+    }}>テスト</p>
+      <p style={{
+      margin: 0
+    }}>テスト</p>
+    </Stack>,
+  args: defaultArgs
+}`,...(S=(T=l.parameters)==null?void 0:T.docs)==null?void 0:S.source}}};var b,B,f;p.parameters={...p.parameters,docs:{...(b=p.parameters)==null?void 0:b.docs,source:{originalSource:`{
+  render: args => <Stack {...args} mt="lg" mr="lg" mb="lg" ml="lg" pt="xxl" pr="xxl" pb="xxl" pl="xxl">
+      <p style={{
+      margin: 0
+    }}>Text</p>
+      <p style={{
+      margin: 0
+    }}>Text</p>
+      <p style={{
+      margin: 0
+    }}>Text</p>
+      <p style={{
+      margin: 0
+    }}>Text</p>
+    </Stack>,
+  args: defaultArgs
+}`,...(f=(B=p.parameters)==null?void 0:B.docs)==null?void 0:f.source}}};var A,w,W;g.parameters={...g.parameters,docs:{...(A=g.parameters)==null?void 0:A.docs,source:{originalSource:`{
+  render: args => <Stack {...args} as={<Box radius="md" backgroundColor="primary" pt="lg" pr="lg" pb="lg" pl="lg" />}>
+      <p style={{
+      margin: 0
+    }}>Text</p>
+      <p style={{
+      margin: 0
+    }}>Text</p>
+      <p style={{
+      margin: 0
+    }}>Text</p>
+      <p style={{
+      margin: 0
+    }}>Text</p>
+    </Stack>,
+  args: defaultArgs
+}`,...(W=(w=g.parameters)==null?void 0:w.docs)==null?void 0:W.source}}};var E,M,P;c.parameters={...c.parameters,docs:{...(E=c.parameters)==null?void 0:E.docs,source:{originalSource:`{
+  render: args => <Stack {...args}>
+      <p>This is a paragraph.</p>
+      <p>This is a paragraph.</p>
+    </Stack>,
+  args: {
+    ...defaultArgs,
+    'data-test-id': 'custom-attribute'
+  }
+}`,...(P=(M=c.parameters)==null?void 0:M.docs)==null?void 0:P.source}}};var O,_,I;d.parameters={...d.parameters,docs:{...(O=d.parameters)==null?void 0:O.docs,source:{originalSource:`{
+  render: args => <>
+      <Stack {...args} width="100%">
+        <p style={{
+        margin: 0
+      }}>width: 100%</p>
+        <p style={{
+        margin: 0
+      }}>Text</p>
+        <p style={{
+        margin: 0
+      }}>Text</p>
+        <p style={{
+        margin: 0
+      }}>Text</p>
+      </Stack>
+
+      <br />
+
+      <Stack {...args} minWidth="500px">
+        <p style={{
+        margin: 0
+      }}>min-width: 500px</p>
+        <p style={{
+        margin: 0
+      }}>Text</p>
+        <p style={{
+        margin: 0
+      }}>Text</p>
+        <p style={{
+        margin: 0
+      }}>Text</p>
+      </Stack>
+
+      <br />
+
+      <Stack {...args} maxWidth="500px">
+        <p style={{
+        margin: 0
+      }}>max-width: 500px</p>
+        <p style={{
+        margin: 0
+      }}>Text</p>
+        <p style={{
+        margin: 0
+      }}>Text</p>
+        <p style={{
+        margin: 0
+      }}>Text</p>
+      </Stack>
+    </>,
+  args: defaultArgs
+}`,...(I=(_=d.parameters)==null?void 0:_.docs)==null?void 0:I.source}}};const H=["Default","Nested","MarkupAsList","PutOnTheEnd","MarginAndPadding","AsBox","CustomDataAttribute","Width"];export{g as AsBox,c as CustomDataAttribute,a as Default,p as MarginAndPadding,i as MarkupAsList,t as Nested,l as PutOnTheEnd,d as Width,H as __namedExportsOrder,G as default};
