1ddc8050afbc44e253b88d226ff8d9ad7f9796a6
hiroki.yokouchi <hiroki.yokouchi@dr-ubie.com>
2024-08-01 14:53:00 +0900

    Add ref test

--- src/components/Pre/Pre.spec.tsx
+++ src/components/Pre/Pre.spec.tsx
@@ -1,7 +1,17 @@
 import { render, screen } from '@testing-library/react';
+import { createRef } from 'react';
 import { Pre } from './Pre';
 
 describe('<Pre>', () => {
+  it('receives ref', () => {
+    const ref = createRef<HTMLSpanElement>();
+
+    render(<Pre ref={ref}>lorem ipsum</Pre>);
+
+    expect(ref.current).not.toBeNull();
+    expect(ref.current?.tagName).toBe('SPAN');
+  });
+
   it('receives data attributes', () => {
     render(<Pre data-testid="pre">lorem ipsum</Pre>);
     const pre = screen.getByTestId('pre');
