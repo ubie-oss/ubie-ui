622a2c96319a9e0f3241991dbf87b95ff28d57ed
hiroki.yokouchi <hiroki.yokouchi@dr-ubie.com>
2024-08-01 14:57:22 +0900

    Allows attributes of textarea element in Textarea Component

--- src/components/TextArea/TextArea.spec.tsx
+++ src/components/TextArea/TextArea.spec.tsx
@@ -1,4 +1,4 @@
-import { render } from '@testing-library/react';
+import { render, screen } from '@testing-library/react';
 import { createRef } from 'react';
 import { TextArea } from './TextArea';
 
@@ -7,11 +7,36 @@ describe('TextArea', () => {
     const ref = createRef<HTMLTextAreaElement>();
 
     render(
-      <TextArea name="test" value="test" ref={ref}>
+      <TextArea
+        name="test"
+        value="test"
+        ref={ref}
+        onChange={() => {
+          /* noop */
+        }}
+      >
         Test
       </TextArea>,
     );
     expect(ref.current).not.toBeNull();
     expect(ref.current?.tagName).toBe('TEXTAREA');
+  });
+
+  it('receives data attributes', async () => {
+    render(
+      <TextArea
+        name="test"
+        value="test"
+        data-testid="textarea"
+        onChange={() => {
+          /* noop */
+        }}
+      >
+        Test
+      </TextArea>,
+    );
+
+    const textarea = await screen.findByTestId('textarea');
+    expect(textarea).toBeInTheDocument();
   });
 });
