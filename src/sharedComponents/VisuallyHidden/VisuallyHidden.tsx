348f11f93a6e07bf5d8f97d343f96973d3a8fc9f
hiroki.yokouchi <hiroki.yokouchi@dr-ubie.com>
2024-08-01 19:16:46 +0900

    Prohibits the specification of components to as

--- src/sharedComponents/VisuallyHidden/VisuallyHidden.tsx
+++ src/sharedComponents/VisuallyHidden/VisuallyHidden.tsx
@@ -1,17 +1,26 @@
-import { ComponentPropsWithRef, ForwardedRef } from 'react';
+import { ComponentPropsWithRef, ElementType, ForwardedRef } from 'react';
 import styles from './VisuallyHidden.module.css';
 import { fixedForwardRef } from '../../utils/component';
 import { DistributiveOmit } from '../../utils/types';
-import type { ElementType } from 'react';
 
-function VisuallyHiddenInner<T extends ElementType>(
+function VisuallyHiddenInner<TAs extends ElementType>(
   props: {
-    as?: T;
-  } & DistributiveOmit<ComponentPropsWithRef<ElementType extends T ? 'span' : T>, 'as'>,
+    /**
+     * レンダリングされる要素。ネイティブ要素のみを許可しています。
+     * TypeScript上ではコンポーネントを指定できてしまいますが、実行時にエラーとなります
+     * @default span
+     */
+    as?: TAs;
+  } & DistributiveOmit<ComponentPropsWithRef<ElementType extends TAs ? 'span' : TAs>, 'ref' | 'className'>,
   // eslint-disable-next-line @typescript-eslint/no-explicit-any
   ref: ForwardedRef<any>,
 ) {
-  const { children, as: Component = 'span', className: _, ...otherProps } = props;
+  const { children, as: Component = 'span', ...otherProps } = props;
+
+  if (typeof (Component as unknown) !== 'string') {
+    throw new Error('as prop only supports native elements.');
+  }
+
   return (
     <Component className={styles.visuallyHidden} {...otherProps} ref={ref}>
       {children}
