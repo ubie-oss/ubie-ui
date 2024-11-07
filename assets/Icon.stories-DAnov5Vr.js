56f07bf603e77c9f8cb22319c32be300b42430c0
takanorip <takanorip@users.noreply.github.com>
2024-11-07 06:11:27 +0000

    deploy: 9db17a6778d32ef805235a8ae6cdd8c05b9eb654

---
+++ assets/Icon.stories-DAnov5Vr.js
@@ -0,0 +1,66 @@
+import{j as s}from"./jsx-runtime-BJwPuJt0.js";import{I as e}from"./Icon-DzaUljVa.js";import{F as d}from"./Flex-D-SJmXqJ.js";import{B as _}from"./Box-BZ8KpU1r.js";import{S as w}from"./Stack-DE5RwiRG.js";import"./index-DX35FyXq.js";import"./ArrowBDownIcon-DHfX99Bt.js";import"./UbieIcon-Dt0Travo.js";import"./UnfoldMoreIcon-N-Pb1J02.js";import"./style-D1Pgu1yr.js";import"./clsx-B-dksMZM.js";const P={component:e},n={icon:"UbieIcon"},o={render:r=>s.jsx(e,{...r}),args:n},a={render:r=>s.jsx(s.Fragment,{children:s.jsxs(d,{alignItems:"center",spacing:"sm",children:[s.jsx(e,{...r,size:"xs"}),s.jsx(e,{...r,size:"sm"}),s.jsx(e,{...r,size:"md"}),s.jsx(e,{...r,size:"lg"}),s.jsx(e,{...r,size:"xl"}),s.jsx(e,{...r,size:"xxl"}),s.jsx(e,{...r,size:"xxxl"}),s.jsx(e,{...r,size:"xxxxl"})]})}),args:n},c={render:r=>s.jsx(s.Fragment,{children:s.jsxs(d,{alignItems:"center",spacing:"sm",children:[s.jsx(e,{...r,size:"2xl"}),s.jsx(e,{...r,size:"3xl"}),s.jsx(e,{...r,size:"4xl"})]})}),args:n},t={render:r=>s.jsxs(d,{alignItems:"center",spacing:"sm",children:[s.jsx(e,{...r,color:"primary"}),s.jsx(e,{...r,color:"accent"}),s.jsx(e,{...r,color:"main"}),s.jsx(e,{...r,color:"sub"}),s.jsx(e,{...r,color:"alert"}),s.jsx(e,{...r,color:"link"}),s.jsx(e,{...r,color:"linkSub"}),s.jsx(e,{...r,color:"disabled"}),s.jsx(_,{pt:"xxs",pr:"xxs",pb:"xxs",pl:"xxs",backgroundColor:"primaryDarken",children:s.jsx(e,{...r,color:"white"})})]}),args:n},i={render:()=>s.jsxs(w,{spacing:"sm",children:[s.jsxs("p",{children:["ラベルや付随する文章を伴わずにアイコン単独で使う場合、アイコンが保つ意味を",s.jsx("code",{children:"label"})," propで付与します",s.jsx("br",{}),"例: アイコンボタンなど"]}),s.jsx("div",{children:s.jsx(e,{icon:"SetupIcon",label:"設定"})})]})},l={render:r=>s.jsx(e,{...r}),args:{...n,"data-testid":"testid"}},x={render:r=>s.jsx(e,{...r}),args:{...n,id:"icon-id","data-testid":"testid"}};var m,p,g;o.parameters={...o.parameters,docs:{...(m=o.parameters)==null?void 0:m.docs,source:{originalSource:`{
+  render: args => <Icon {...args} />,
+  args: defaultArgs
+}`,...(g=(p=o.parameters)==null?void 0:p.docs)==null?void 0:g.source}}};var u,I,j;a.parameters={...a.parameters,docs:{...(u=a.parameters)==null?void 0:u.docs,source:{originalSource:`{
+  render: args => <>
+      <Flex alignItems="center" spacing="sm">
+        <Icon {...args} size="xs" />
+        <Icon {...args} size="sm" />
+        <Icon {...args} size="md" />
+        <Icon {...args} size="lg" />
+        <Icon {...args} size="xl" />
+        <Icon {...args} size="xxl" />
+        <Icon {...args} size="xxxl" />
+        <Icon {...args} size="xxxxl" />
+      </Flex>
+    </>,
+  args: defaultArgs
+}`,...(j=(I=a.parameters)==null?void 0:I.docs)==null?void 0:j.source}}};var z,b,S;c.parameters={...c.parameters,docs:{...(z=c.parameters)==null?void 0:z.docs,source:{originalSource:`{
+  render: args => <>
+      <Flex alignItems="center" spacing="sm">
+        <Icon {...args} size="2xl" />
+        <Icon {...args} size="3xl" />
+        <Icon {...args} size="4xl" />
+      </Flex>
+    </>,
+  args: defaultArgs
+}`,...(S=(b=c.parameters)==null?void 0:b.docs)==null?void 0:S.source}}};var h,f,k;t.parameters={...t.parameters,docs:{...(h=t.parameters)==null?void 0:h.docs,source:{originalSource:`{
+  render: args => <Flex alignItems="center" spacing="sm">
+      <Icon {...args} color="primary" />
+      <Icon {...args} color="accent" />
+      <Icon {...args} color="main" />
+      <Icon {...args} color="sub" />
+      <Icon {...args} color="alert" />
+      <Icon {...args} color="link" />
+      <Icon {...args} color="linkSub" />
+      <Icon {...args} color="disabled" />
+      <Box pt="xxs" pr="xxs" pb="xxs" pl="xxs" backgroundColor="primaryDarken">
+        <Icon {...args} color="white" />
+      </Box>
+    </Flex>,
+  args: defaultArgs
+}`,...(k=(f=t.parameters)==null?void 0:f.docs)==null?void 0:k.source}}};var A,F,C;i.parameters={...i.parameters,docs:{...(A=i.parameters)==null?void 0:A.docs,source:{originalSource:`{
+  render: () => <Stack spacing="sm">
+      <p>
+        ラベルや付随する文章を伴わずにアイコン単独で使う場合、アイコンが保つ意味を<code>label</code> propで付与します
+        <br />
+        例: アイコンボタンなど
+      </p>
+      <div>
+        <Icon icon="SetupIcon" label="設定" />
+      </div>
+    </Stack>
+}`,...(C=(F=i.parameters)==null?void 0:F.docs)==null?void 0:C.source}}};var y,D,B;l.parameters={...l.parameters,docs:{...(y=l.parameters)==null?void 0:y.docs,source:{originalSource:`{
+  render: args => <Icon {...args} />,
+  args: {
+    ...defaultArgs,
+    'data-testid': 'testid'
+  }
+}`,...(B=(D=l.parameters)==null?void 0:D.docs)==null?void 0:B.source}}};var W,v,O;x.parameters={...x.parameters,docs:{...(W=x.parameters)==null?void 0:W.docs,source:{originalSource:`{
+  render: args => <Icon {...args} />,
+  args: {
+    ...defaultArgs,
+    id: 'icon-id',
+    'data-testid': 'testid'
+  }
+}`,...(O=(v=x.parameters)==null?void 0:v.docs)==null?void 0:O.source}}};const Q=["Default","Size","SizeAlias","Color","CaseOnlyIcon","WithCustomDataAttribute","WithId"];export{i as CaseOnlyIcon,t as Color,o as Default,a as Size,c as SizeAlias,l as WithCustomDataAttribute,x as WithId,Q as __namedExportsOrder,P as default};
